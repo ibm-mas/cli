c4c296671cac594b51b3964f3d1e528be537f5fc
David Parker <parkerda@uk.ibm.com>
2022-05-22 01:26:23 +0100

    Update initbuild.sh

--- build/bin/initbuild.sh
+++ build/bin/initbuild.sh
@@ -91,6 +91,9 @@ fi
 # 2. Tweak version string for pre-release builds
 # -----------------------------------------------------------------------------
 if [[ "${GITHUB_REF_TYPE}" == "branch" ]]; then
+    echo "Current version (from $VERSION_FILE) = $(cat $VERSION_FILE)"
+    echo -n "After bump: "
+    semver bump prerel pre.$GITHUB_REF_NAME $(cat $VERSION_FILE)
     semver bump prerel pre.$GITHUB_REF_NAME $(cat $VERSION_FILE) > $VERSION_FILE
     echo "Pre-release build: $(cat $VERSION_FILE)"
 fi
