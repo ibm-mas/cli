b84daa3a7943822134a3e34c1854f718da012a19
David Parker <parkerda@uk.ibm.com>
2022-08-04 11:29:20 +0100

    Fix "file exists" condition checks

--- image/cli/install-ansible/install-ansible-collections.sh
+++ image/cli/install-ansible/install-ansible-collections.sh
@@ -2,12 +2,12 @@
 
 ansible-galaxy collection install -r /tmp/install-ansible/requirements.yml -p $ANSIBLE_COLLECTIONS_PATH
 
-if [[ -n /tmp/install/ibm-mas-devops.tar.gz ]]
+if [[ -e /tmp/install/ibm-mas-devops.tar.gz ]]
 then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
 else ansible-galaxy collection install ibm.mas_devops -p $ANSIBLE_COLLECTIONS_PATH
 fi
 
-if [[ -n /tmp/install/ibm-mas-airgap.tar.gz ]]
+if [[ -e /tmp/install/ibm-mas-airgap.tar.gz ]]
 then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_airgap.tar.gz --force --no-deps -p $ANSIBLE_COLLECTIONS_PATH
 else ansible-galaxy collection install ibm.mas_airgap -p $ANSIBLE_COLLECTIONS_PATH
 fi
