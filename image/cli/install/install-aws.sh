4a891aac84e0f7162f94bae238429cad0a5ee59d
David Parker <parkerda@uk.ibm.com>
2022-12-22 12:43:45 +0000

    Make tar and unzip less verbose (reduce log spam)

--- image/cli/install/install-aws.sh
+++ image/cli/install/install-aws.sh
@@ -4,7 +4,7 @@
 set -e
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 ./aws/install
 
 rm -rf aws
--- image/cli/install/install-skopeo.sh
+++ image/cli/install/install-skopeo.sh
@@ -6,6 +6,6 @@ set -e
 dnf install skopeo hostname httpd-tools -y
 dnf clean all
 wget -q https://github.com/IBM/cloud-pak-cli/releases/download/v3.17.0/cloudctl-linux-amd64.tar.gz
-tar -xvf cloudctl-linux-amd64.tar.gz
+tar -xf cloudctl-linux-amd64.tar.gz
 mv cloudctl-linux-amd64 /usr/bin/cloudctl
 rm cloudctl-linux-amd64.tar.gz
