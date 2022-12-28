4842df5f747c46cf5b1e3639596e067458009f98
David Parker <parkerda@uk.ibm.com>
2022-12-28 17:39:31 +0000

    Fix docker build

--- image/cli/install-ansible/install-ansible-collections.sh
+++ image/cli/install-ansible/install-ansible-collections.sh
@@ -2,7 +2,7 @@
 
 ansible-galaxy collection install -r /tmp/install-ansible/requirements.yml -p $ANSIBLE_COLLECTIONS_PATH
 
-if [[ -e /tmp/install-ansible/ibm-mas_devops-deccat.tar.gz ]]
-then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops-deccat.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
+if [[ -e /tmp/install-ansible/ibm-mas_devops.tar.gz ]]
+then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
 else ansible-galaxy collection install ibm.mas_devops
 fi
