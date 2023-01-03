662ddd96b8f36b61803147e5525d9cbf6670d9a8
Alexandre Quinteiro <alefq@Alexandres-MBP.lan>
2023-01-03 00:14:55 -0300

    [minor] Build and upload pipelinerun artifacts

--- build/bin/build-tekton.sh
+++ build/bin/build-tekton.sh
@@ -10,10 +10,16 @@ if [ "$DEV_MODE" != "true" ]; then
   TASK_FILES=$GITHUB_WORKSPACE/tekton/tasks/*.yml
   PIPELINE_FILES=$GITHUB_WORKSPACE/tekton/pipelines/*.yml
   TARGET_FILE=$GITHUB_WORKSPACE/image/cli/mascli/templates/ibm-mas-tekton.yml
+  
+  PIPELINERUN_FILES=$GITHUB_WORKSPACE/tekton/pipelinerun/*.j2
+  TARGET_PIPELINERUN_DIRECTORY=$GITHUB_WORKSPACE/image/cli/mascli/templates/
 else
   TASK_FILES=$DIR/../../tekton/tasks/*.yml
   PIPELINE_FILES=$DIR/../../tekton/pipelines/*.yml
   TARGET_FILE=$DIR/../../image/cli/mascli/templates/ibm-mas-tekton.yml
+  
+  PIPELINERUN_TEMPLATES=$DIR../../tekton/pipelinerun/templates/*.j2
+  TARGET_PIPELINERUN_DIRECTORY=$DIR/../../image/cli/mascli/templates
 fi
 
 echo "" > $TARGET_FILE
@@ -44,3 +50,6 @@ mv $TARGET_FILE.txt $TARGET_FILE
 if [ "$DEV_MODE" != "true" ]; then
   cat $TARGET_FILE
 fi
+
+echo "Moving PipelineRun templates to $TARGET_PIPELINERUN_DIRECTORY"
+cp PIPELINERUN_TEMPLATES TARGET_PIPELINERUN_DIRECTORY
