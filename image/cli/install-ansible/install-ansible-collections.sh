27fe2a3771535bc67e0a2da52c29ae0c80747e20
Terence Quinn <quinnt@us.ibm.com>
2023-01-11 20:03:29 -0600

    [patch] use dev version of ansible devops

--- image/cli/install-ansible/install-ansible-collections.sh
+++ image/cli/install-ansible/install-ansible-collections.sh
@@ -4,5 +4,5 @@ ansible-galaxy collection install -r /tmp/install-ansible/requirements.yml -p $A
 
 if [[ -e /tmp/install-ansible/ibm-mas_devops.tar.gz ]]
 then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
-else ansible-galaxy collection install ibm.mas_devops
+else ansible-galaxy collection install ibm.mas_devops-12.7.0-pre.jancat
 fi
