ca488b1ef1dd61e32b8972608d2686b840741338
David Parker <parkerda@uk.ibm.com>
2023-01-18 12:07:37 +0000

    [patch] Fix permission errors in container image

--- image/cli/install/install-ibmcloud.sh
+++ image/cli/install/install-ibmcloud.sh
@@ -6,7 +6,7 @@ set -e
 wget -q https://download.clis.cloud.ibm.com/ibm-cloud-cli/2.12.1/IBM_Cloud_CLI_2.12.1_amd64.tar.gz
 tar -xzf IBM_Cloud_CLI_2.12.1_amd64.tar.gz
 mv Bluemix_CLI/bin/ibmcloud /usr/local/bin/
-rm -rf Bluemix_CLI IBM_Cloud_CLI_2.3.0_amd64.tar.gz
+rm -rf Bluemix_CLI IBM_Cloud_CLI_2.12.1_amd64.tar.gz
 ibmcloud plugin repo-plugins -r 'IBM Cloud'
 ibmcloud plugin install container-service
 ibmcloud plugin install container-registry
