1250b475510dc2db47f62d1217ed85e261d39575
David Parker <parkerda@uk.ibm.com>
2023-01-20 19:07:25 +0000

    [patch] Generate additional debug for RBAC (#146)

--- image/cli/app-root/run-playbook.sh
+++ image/cli/app-root/run-playbook.sh
@@ -8,7 +8,11 @@ if [ -e "/workspace/entitlement/entitlement.lic" ]; then
   cp /workspace/entitlement/entitlement.lic /workspace/configs/entitlement.lic
 fi
 
+# Debug
 source /opt/app-root/src/env.sh
+oc whoami
+oc auth can-i --list
+
 ansible-playbook "$@"
 rc=$?
 python3 /opt/app-root/src/save-junit-to-mongo.py
