62ccbe97bec9956f161c6ada4e29a64b495c7a2c
David Parker <parkerda@uk.ibm.com>
2023-01-31 12:36:25 +0000

    Fix wrong file extension

--- image/cli/install-ansible/install-ansible-collections.sh
+++ image/cli/install-ansible/install-ansible-collections.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ansible-galaxy collection install -r /tmp/install-ansible/requirements.yaml -p $ANSIBLE_COLLECTIONS_PATH
+ansible-galaxy collection install -r /tmp/install-ansible/requirements.yml -p $ANSIBLE_COLLECTIONS_PATH
 
 if [[ -e /tmp/install-ansible/ibm-mas_devops.tar.gz ]]
 then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
