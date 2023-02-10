2456a20bd29d7e1270818bd33c659bb287e1c53d
Rawa Resul <rawa.resul@gmail.com>
2023-02-10 15:12:34 +0000

    [patch] update run-playbook script

--- image/cli/app-root/run-playbook.sh
+++ image/cli/app-root/run-playbook.sh
@@ -14,7 +14,7 @@ source /opt/app-root/src/env.sh
 # oc whoami
 # oc auth can-i --list
 
-ansible-playbook "$@"
+ansible-playbook ibm.mas_devops."$@"
 rc=$?
 python3 /opt/app-root/src/save-junit-to-mongo.py
 exit $rc
