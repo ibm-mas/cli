ff751ffb7e39919438e11037809d777cef3a8544
Caio Pereira <caiofcp@br.ibm.com>
2023-04-10 09:30:03 -0300

    [patch] fixing helm permission

--- image/cli/install/install-helm.sh
+++ image/cli/install/install-helm.sh
@@ -6,3 +6,5 @@ set -e
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+chown default:root /usr/local/bin/helm
+chmod  g=u /usr/local/bin/helm
