6369a82041d11a4a2cc1af7c7adde8a6c4bc72fb
David Parker <parkerda@uk.ibm.com>
2023-04-27 16:10:37 +0100

    [minor] Add uninstall/upgrade-after-install pipelines (#265)

--- build/bin/build-tekton.sh
+++ build/bin/build-tekton.sh
@@ -13,6 +13,7 @@ if [ "$DEV_MODE" != "true" ]; then
   PIPELINE_FILES=$TARGET_DIR/pipelines/*.yaml
 
   TARGET_FILE=$GITHUB_WORKSPACE/tekton/target/ibm-mas-tekton.yaml
+  TARGET_FILE_FVT=$GITHUB_WORKSPACE/tekton/target/ibm-mas-tekton-fvt.yaml
   TARGET_FILE_IN_CLI=$GITHUB_WORKSPACE/image/cli/mascli/templates/ibm-mas-tekton.yaml
 else
   TARGET_DIR=$DIR/../../tekton/target
@@ -22,40 +23,62 @@ else
   PIPELINE_FILES=$TARGET_DIR/pipelines/*.yaml
 
   TARGET_FILE=$DIR/../../tekton/target/ibm-mas-tekton.yaml
+  TARGET_FILE_FVT=$DIR/../../tekton/target/ibm-mas-tekton-fvt.yaml
   TARGET_FILE_IN_CLI=$DIR/../../image/cli/mascli/templates/ibm-mas-tekton.yaml
 fi
 
+# 1. Generate tasks and pipelines
+# -----------------------------------------------------------------------------
 ansible-playbook tekton/generate-tekton-tasks.yml
 ansible-playbook tekton/generate-tekton-pipelines.yml
-ansible-playbook tekton/generate-tekton-pipelineruns.yml
-
-# Special case, has extra non-standard build logic
-ansible-playbook tekton/generate-tekton-upgrade-with-fvt.yml
 
 
+# 2. Generate ibm-mas-tekton.yaml and ibm-mas-tekton-fvt.yaml
+# -----------------------------------------------------------------------------
+echo "Creating tekton installers"
 echo "" > $TARGET_FILE
+echo "" > $TARGET_FILE_FVT
 
-echo "Creating tekton installer ($TARGET_FILE)"
+
+function addToFile() {
+  src_file=$1
+  dst_file=$2
+  printf '[%-23s] %s\n' $(basename $dst_file) $(basename $src_file)
+  echo "# --------------------------------------------------------------------------------" >> $dst_file
+  echo "# $src_file" >> $dst_file
+  echo "# --------------------------------------------------------------------------------" >> $dst_file
+  cat $src_file >> $dst_file
+
+}
+
 for FILE in $TASK_FILES; do
-  echo " - Adding Task: $FILE"
-  echo "# --------------------------------------------------------------------------------" >> $TARGET_FILE
-  echo "# $FILE" >> $TARGET_FILE
-  echo "# --------------------------------------------------------------------------------" >> $TARGET_FILE
-  cat $FILE >> $TARGET_FILE
+  FILE_NAME=$(basename $FILE)
+  addToFile $FILE $TARGET_FILE_FVT
+
+  if [[ ! "$FILE_NAME" == fvt-* ]]
+  then
+    addToFile $FILE $TARGET_FILE
+  fi
 done
 
 for FILE in $PIPELINE_FILES; do
-  echo " - Adding Pipeline: $FILE"
-  echo "# --------------------------------------------------------------------------------" >> $TARGET_FILE
-  echo "# $FILE" >> $TARGET_FILE
-  echo "# --------------------------------------------------------------------------------" >> $TARGET_FILE
-  cat $FILE >> $TARGET_FILE
+  FILE_NAME=$(basename $FILE)
+  addToFile $FILE $TARGET_FILE_FVT
+
+  if [[ ! "$FILE_NAME" == fvt-* ]] && [[ ! "$FILE_NAME" == *-after-install.yaml ]] && [[ ! "$FILE_NAME" == *-with-fvt.yaml ]]
+  then
+    addToFile $FILE $TARGET_FILE
+  fi
 done
 
+# What a very long winded way of doing in-place sed! I'm sure someone did it this way for a reason?
 sed "s#quay.io/ibmmas/cli:latest#quay.io/ibmmas/cli:$VERSION#g" $TARGET_FILE > $TARGET_FILE.txt
-
+sed "s#quay.io/ibmmas/cli:latest#quay.io/ibmmas/cli:$VERSION#g" $TARGET_FILE_FVT > $TARGET_FILE_FVT.txt
 rm $TARGET_FILE
+rm $TARGET_FILE_FVT
 mv $TARGET_FILE.txt $TARGET_FILE
+mv $TARGET_FILE_FVT.txt $TARGET_FILE_FVT
+
 cp $TARGET_FILE $TARGET_FILE_IN_CLI
 
 # Extra debug for Travis builds
