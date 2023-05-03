7208c15b95d2b78feff5f3636cb00f9c2b9f1f9f
André Marcelino <andrercm@br.ibm.com>
2023-05-03 15:18:16 -0300

    [patch] remove test-generate-all.sh

--- tekton/test-generate-all.sh
+++
@@ -1,4 +0,0 @@
-ansible-playbook generate-tekton-tasks.yml
-ansible-playbook generate-tekton-pipelineruns.yml
-ansible-playbook generate-tekton-pipelines.yml
-ansible-playbook generate-tekton-upgrade-with-fvt.yml
