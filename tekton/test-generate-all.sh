bc726990b95503a4bc7671775552356d4e178c49
leo-miran <105313348+leo-miran@users.noreply.github.com>
2023-05-04 06:29:02 -0300

    [patch] MAS Industry Solutions FVT tidy-up (#273)

--- tekton/test-generate-all.sh
+++ tekton/test-generate-all.sh
@@ -1,4 +1,2 @@
 ansible-playbook generate-tekton-tasks.yml
-ansible-playbook generate-tekton-pipelineruns.yml
 ansible-playbook generate-tekton-pipelines.yml
-ansible-playbook generate-tekton-upgrade-with-fvt.yml
