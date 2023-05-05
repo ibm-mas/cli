b59719e1ffb9c8f6156d08fe36bd9802ca772617
André Marcelino <andrercm@br.ibm.com>
2023-05-05 16:49:59 -0300

    [patch] try to pull from artifactory if use branch

--- image/cli/install-ansible/install-ansible-collections.sh
+++ image/cli/install-ansible/install-ansible-collections.sh
@@ -2,7 +2,15 @@
 
 ansible-galaxy collection install -r /tmp/install-ansible/requirements.yml -p $ANSIBLE_COLLECTIONS_PATH
 
-if [[ -e /tmp/install-ansible/ibm-mas_devops.tar.gz ]]
-then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
-else ansible-galaxy collection install ibm.mas_devops
+if [[ -e /tmp/install-ansible/ibm-mas_devops.tar.gz ]]; then
+    ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
+elif [[ "${GITHUB_REF_TYPE}" == "branch" ]]; then
+    ANSIBLE_COLLECTION_FILE=ibm-mas_devops-13.7.0-pre.improve-build-system.tar.gz
+    echo $ANSIBLE_COLLECTION_FILE
+    wget --header="Authorization:Bearer $ARTIFACTORY_TOKEN" $ARTIFACTORY_GENERIC_RELEASE_URL/ibm-mas/ansible-devops/$ANSIBLE_COLLECTION_FILE
+    ansible-galaxy collection install $ANSIBLE_COLLECTION_FILE -p $ANSIBLE_COLLECTIONS_PATH
+    # mv $ANSIBLE_COLLECTION_FILE $ANSIBLE_COLLECTIONS_PATH/ibm-mas_devops.tar.gz 
+    # ansible-galaxy collection install $ANSIBLE_COLLECTIONS_PATH/ibm-mas_devops.tar.gz
+else
+    ansible-galaxy collection install ibm.mas_devops
 fi
