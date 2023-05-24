29a663943f5a86fcaa82fbec611afe6f66f203e6
David Parker <parkerda@uk.ibm.com>
2023-05-24 15:05:56 +0100

    Updates

--- image/cli/mascli/must-gather/summarizer/mg/subscriptions.py
+++ image/cli/mascli/must-gather/summarizer/mg/subscriptions.py
@@ -5,58 +5,62 @@ from prettytable import PrettyTable
 
 from mg.utils import printHeader
 
-def process_subscription( output_dir, subscription, subscriptions_table ):
-  subscriptionnamespace = subscription.rsplit()[0]
-  subscriptionname = subscription.rsplit()[1]
-
-  getSubscriptionOutput =  subprocess.run( "oc get subscription " + subscriptionname + " -n " + subscriptionnamespace + " -o yaml" , shell=True, capture_output=True, text=True)
-  subscription_yaml =  yaml.load( getSubscriptionOutput.stdout, Loader=Loader )
-
+def process_subscription( output_dir, subscriptionYaml, ipYaml, subscriptions_table ):
   # Extract the conditions we are interested in
-  sub_condition_types=['CatalogSourcesUnhealthy']
-  sub_condition_statuses= {x['type']:{
-    "status": x[ 'status']} for x in subscription_yaml['status']['conditions'] if x['type'] in sub_condition_types}
+  sub_condition_types=["CatalogSourcesUnhealthy"]
+  sub_condition_statuses= {x["type"]:{
+    "status": x[ "status"]} for x in subscriptionYaml["status"]["conditions"] if x["type"] in sub_condition_types}
   sub_condition_statuses
 
-  installplanname=subscription_yaml['status']['installplan']['name']
+  installplanname=subscriptionYaml["status"]["installplan"]["name"]
 
-  #Lets check if we have an installplan as referenced in the subscription
-  installplan_status="na"
 
-  getInstallPlanOutput =  subprocess.run( "oc get installplan " + installplanname + " -n " + subscriptionnamespace + " -o yaml" , shell=True, capture_output=True, text=True)
-  installplan_yaml =  yaml.load( getInstallPlanOutput.stdout, Loader=Loader)
-
-  if str(installplan_yaml) != "None":
-    ip_condition_types=['Installed']
-    ip_condition_statuses= {x['type']:{
-      "status": x[ 'status']} for x in installplan_yaml['status']['conditions'] if x['type'] in ip_condition_types}
-    ip_condition_statuses
-    ip_status= ip_condition_statuses['Installed']['status']
+  if ipYaml is not None:
+    ip_status = ipYaml["status"]["phase"]
   else:
-    ip_status="UNKNOWN"
+    ip_status="<unknown>"
 
-  subscriptions_table.add_row([subscriptionname,
-                               subscriptionnamespace,
-                               subscription_yaml['spec']['channel'],
-                               subscription_yaml['spec']['source'],
-                               subscription_yaml['status']['installedCSV'],
-                               subscription_yaml['spec']['installPlanApproval'],
-                               sub_condition_statuses['CatalogSourcesUnhealthy']['status'],
-                               subscription_yaml['status']['installplan']['name'],
-                               ip_status])
+  if "installPlanApproval" in subscriptionYaml["spec"]:
+    approval = subscriptionYaml["spec"]["installPlanApproval"]
+  else:
+    approval = "<undefined>"
+
+  subscriptions_table.add_row([
+    subscriptionYaml["metadata"]["namespace"],
+    subscriptionYaml["metadata"]["name"],
+    subscriptionYaml["spec"]["channel"],
+    subscriptionYaml["spec"]["source"],
+    subscriptionYaml["status"]["installedCSV"],
+    approval,
+    sub_condition_statuses["CatalogSourcesUnhealthy"]["status"],
+    subscriptionYaml["status"]["installplan"]["name"],
+    ip_status
+  ])
 
 
 def summarize( output_dir):
-  subscriptions_file=output_dir + "/resources/_cluster/subscriptions.txt"
+  subsFile = output_dir + "/resources/_cluster/subscriptions/all-namespaces.yaml"
+  ipsFile = output_dir + "/resources/_cluster/installplans/all-namespaces.yaml"
+
+  with open(subsFile, "r") as file:
+    subs=yaml.safe_load( file )
+
+  with open(ipsFile, "r") as file:
+    ips=yaml.safe_load( file )
+
+  installPlansByName={}
+  for ip in ips["items"]:
+    installPlansByName[ip["metadata"]["name"]]=ip
+
   subscriptions_table=PrettyTable()
-  subscriptions_table.field_names = [ "Name", "Namespace", "Channel", "Source", "Installed CSV" ,"Approval", "CS Health", 'Install Plan', 'Installed' ]
-  header=True
-  with open( subscriptions_file ) as file:
-    for subscription in file:
-      if header == True:
-        header=False
-      else:
-         process_subscription( output_dir, subscription, subscriptions_table )
+  subscriptions_table.field_names = [ "Namespace", "Name", "Channel", "Source", "Installed CSV" ,"Approval", "CS Health", "Install Plan", "Install Phase"]
+
+  for sub in subs["items"]:
+    if "installplan" in sub["status"] and sub["status"]["installplan"]["name"] in installPlansByName:
+      ip = installPlansByName[sub["status"]["installplan"]["name"]]
+    else:
+      ip = None
+    process_subscription( output_dir, sub, ip, subscriptions_table )
 
   printHeader( "Subscriptions" )
   subscriptions_table.align = "l"
