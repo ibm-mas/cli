5bb6887a3d71cc9a6a78c68f2cb9dc6f16554e65
Caio Pereira <caiofcp@br.ibm.com>
2023-08-04 10:41:09 -0300

    [patch] install rosa cli

---
+++ image/cli/install/install-rosa.sh
@@ -0,0 +1,7 @@
+# Install ROSA Cli
+set -e
+
+wget -q https://mirror.openshift.com/pub/openshift-v4/clients/rosa/latest/rosa-linux.tar.gz?extIdCarryOver=true&intcmp=701f20000012k69AAA&sc_cid=701f2000001Css5AAC
+tar -xzf tar xvf rosa-linux.tar.gz
+sudo mv rosa /usr/local/bin/rosa
+rosa version
