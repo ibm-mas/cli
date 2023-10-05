d09e7be64e27ffd3c066a9508325a819f92de855
Mrunalini Kankariya <mrunalini.kankariya2@ibm.com>
2023-10-05 09:47:14 +0530

    [minor] Added changes to test with Ansible changed branch

--- image/cli/install-ansible/install-ansible-collections.sh
+++ image/cli/install-ansible/install-ansible-collections.sh
@@ -2,7 +2,15 @@
 
 ansible-galaxy collection install -r /tmp/install-ansible/requirements.yml -p $ANSIBLE_COLLECTIONS_PATH
 
-if [[ -e /tmp/install-ansible/ibm-mas_devops.tar.gz ]]
-then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
-else ansible-galaxy collection install ibm.mas_devops
-fi
+git clone -b MASCORE-24 https://github.com/YuvrajVansure/ansible-devops.git
+export ANSIBLE_DEVOPS_PATH=ansible-devops/ibm/mas_devops
+
+cd $ANSIBLE_DEVOPS_PATH
+ansible-galaxy collection build
+ansible-galaxy collection install ibm-mas_devops-*.tar.gz --ignore-certs --force
+
+rm ibm-mas_devops-*.tar.gz
+#if [[ -e /tmp/install-ansible/ibm-mas_devops.tar.gz ]]
+#then ansible-galaxy collection install /tmp/install-ansible/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
+#else ansible-galaxy collection install ibm.mas_devops
+#fi
