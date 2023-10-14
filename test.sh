bf99245ce2cfde83790efb2bfaa384d46c7fd415
David Parker <parkerda@uk.ibm.com>
2023-10-14 20:26:26 +0100

    [patch] Remove Galaxy build icon
    Changes the Ansible Galaxy APIs have rendered the old "latest version" shield broken.

--- test.sh
+++
@@ -1,6 +0,0 @@
-cmd="mas install -c $MAS_CATALOG_VERSION \\
-  --license-file $SLS_LICENSE_FILE_LOCAL \\
-  --uds-email $UDS_CONTACT_EMAIL --uds-firstname $UDS_CONTACT_FIRSTNAME"
-EOM
-
-echo "$cmd"
