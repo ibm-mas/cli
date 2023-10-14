ac4d1ba4b183f05eeba31d3555d9ee3dfcfcab48
David Parker <parkerda@uk.ibm.com>
2023-10-14 17:04:25 +0100

    [patch] Various small improvements & bug fixes (#553)

---
+++ test.sh
@@ -0,0 +1,6 @@
+cmd="mas install -c $MAS_CATALOG_VERSION \\
+  --license-file $SLS_LICENSE_FILE_LOCAL \\
+  --uds-email $UDS_CONTACT_EMAIL --uds-firstname $UDS_CONTACT_FIRSTNAME"
+EOM
+
+echo "$cmd"
