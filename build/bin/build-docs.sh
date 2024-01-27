2bd071630c8ed26bfeb31f27fd7429630294fea3
David Parker <parkerda@uk.ibm.com>
2024-01-27 11:03:00 +0000

    Final prep

--- build/bin/build-docs.sh
+++ build/bin/build-docs.sh
@@ -3,11 +3,11 @@
 # Update all the placeholders in the doc source
 # Make sure not to commit these changes if you run this script locally
 find docs -type f -name '*.md' -exec sed -i \
-  -e 's/@@CLI_LATEST_VERSION@@/7.10.4/g' \
+  -e 's/@@CLI_LATEST_VERSION@@/7.14.0/g' \
   -e 's/@@MAS_PREVIOUS_CHANNEL@@/8.10.x/g' \
-  -e 's/@@MAS_PREVIOUS_CATALOG@@/v8-231031-amd64/g' \
+  -e 's/@@MAS_PREVIOUS_CATALOG@@/v8-231228-amd64/g' \
   -e 's/@@MAS_LATEST_CHANNEL@@/8.11.x/g' \
-  -e 's/@@MAS_LATEST_CATALOG@@/v8-231128-amd64/g' \
+  -e 's/@@MAS_LATEST_CATALOG@@/v8-240130-amd64/g' \
   {} \;
 
 python -m pip install -q mkdocs mkdocs-redirects
