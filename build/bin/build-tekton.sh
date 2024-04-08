df82305b524b185a116a6eefca55881bbb4ac0e8
Yuvraj Vansure <yuvraj.vansure1.ibm.com>
2024-04-08 15:01:54 +0530

    [patch] code refactored

--- build/bin/build-tekton.sh
+++ build/bin/build-tekton.sh
@@ -17,7 +17,7 @@ if [ "$DEV_MODE" != "true" ]; then
   TARGET_FILE_IN_CLI=$GITHUB_WORKSPACE/image/cli/mascli/templates/ibm-mas-tekton.yaml
 else
   TARGET_DIR=$DIR/../../tekton/target
-  VERSION=${VERSION:-6.1.0-pre.deprov}
+  VERSION=${VERSION:-18.17.3-pre.MASCORE-2241}
 
   TASK_FILES=$TARGET_DIR/tasks/*.yaml
   PIPELINE_FILES=$TARGET_DIR/pipelines/*.yaml
