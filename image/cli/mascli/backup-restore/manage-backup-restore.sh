7fc72e7eb530713724b86a5bcf2bdfa9a33829fa
Sarika Budhiraja <sbudhira@us.ibm.com>
2024-04-28 22:11:49 -0400

    [patch] Update manage-backup-restore.sh

--- image/cli/mascli/backup-restore/manage-backup-restore.sh
+++ image/cli/mascli/backup-restore/manage-backup-restore.sh
@@ -143,7 +143,8 @@ if [ "$MODE" == "backup" ]; then
     echo "Starting MAS Managebackup using the instance id $MAS_INSTANCE_ID to $BACKUP_FOLDER"
     mkdir -p $BACKUP_FOLDER
     backupSingleResource Subscription ibm-mas-manage $MAS_MANAGE_NAMESPACE
-    backupSingleResource OperatorGroup ibm-manage-operatorgroup $MAS_MANAGE_NAMESPACE
+    OPERATOR_GROUP=$(oc get operatorgroup -n mas-$MAS_INSTANCE_ID-manage -o name)
+    backupSingleResource OperatorGroup $OPERATOR_GROUP $MAS_MANAGE_NAMESPACE
     backupSingleResource Secret ibm-entitlement $MAS_MANAGE_NAMESPACE
     backupSingleResource Secret $MAS_WORKSPACE_ID-manage-encryptionsecret $MAS_MANAGE_NAMESPACE
     backupSingleResource Secret $MAS_WORKSPACE_ID-manage-encryptionsecret-operator $MAS_MANAGE_NAMESPACE
