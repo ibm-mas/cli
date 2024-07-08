cbaa37453592440e824489295844c07b8d931900
David Parker <parkerda@uk.ibm.com>
2024-07-08 17:53:50 +0100

    [minor] Add (beta) standalone applications for Windows, Linux, and Mac (#1102)

---
+++ python/test/test_help.py
@@ -0,0 +1,42 @@
+import pytest
+
+from mas.cli.install.app import InstallApp
+from mas.cli.upgrade.app import UpgradeApp
+from mas.cli.uninstall.app import UninstallApp
+
+from prompt_toolkit.application import create_app_session
+from prompt_toolkit.output import DummyOutput
+
+def testInstallHelp():
+    """
+    Should exit with RC = 0 after printing help message
+    """
+    with create_app_session(output=DummyOutput()):
+        with pytest.raises(SystemExit) as e:
+            app = InstallApp()
+            app.install(argv=["--help"])
+        assert e.type == SystemExit
+        assert e.value.code == 0
+
+
+def testUninstallHelp():
+    """
+    Should exit with RC = 0 after printing help message
+    """
+    with create_app_session(output=DummyOutput()):
+        with pytest.raises(SystemExit) as e:
+            app = UninstallApp()
+            app.uninstall(argv=["--help"])
+        assert e.type == SystemExit
+        assert e.value.code == 0
+
+def testUpgradeHelp():
+    """
+    Should exit with RC = 0 after printing help message
+    """
+    with create_app_session(output=DummyOutput()):
+        with pytest.raises(SystemExit) as e:
+            app = UpgradeApp()
+            app.upgrade(argv=["--help"])
+        assert e.type == SystemExit
+        assert e.value.code == 0
