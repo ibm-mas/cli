d19928a7b263380494e4d15918faa6c412fa285d
Harsh Tamakuwala <Harsh.Tamakuwala@ibm.com>
2024-07-11 12:16:22 +0530

    [patch] fixed filecheck for apps

--- python/src/mas/cli/install/settings/additionalConfigs.py
+++ python/src/mas/cli/install/settings/additionalConfigs.py
@@ -173,7 +173,7 @@ class AdditionalConfigsMixin():
             for app in apps:
                 if self.getParam(app) != "":
                     for file in ["ca.crt", "tls.cert", "tls.key"]:
-                        if file not in glob(f'{apps[app]["dir"]}/*'):
+                        if file not in map(path.basename, glob(f'{apps[app]["dir"]}/*')):
                             self.fatalError(f'{file} is not present in {apps[app]["dir"]}')
                     for ext in extensions:
                         certsSecret = self.addFilesToSecret(certsSecret, apps[app]["dir"], ext, apps[app]["keyPrefix"])
