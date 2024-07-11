b6ead810dcbdf36c8e17a63066cdd07bb90ecc1a
Matheus Cardoso de Medeiros <Matheus.Cardoso@ibm.com>
2024-07-11 09:13:47 -0300

    [patch] deleted catalog warning

--- python/src/mas/cli/install/app.py
+++ python/src/mas/cli/install/app.py
@@ -59,8 +59,8 @@ class InstallApp(BaseApp, InstallSettingsMixin, InstallSummarizerMixin, ConfigGe
                 catalogId = m.group("catalogId")
             elif re.match(r".+v8-amd64", catalogDisplayName):
                 catalogId = "v8-amd64"
-            else:
-                self.fatalError(f"IBM Maximo Operator Catalog is already installed on this cluster. However, it is not possible to identify its version. If you wish to install a new MAS instance using the {self.getParam('mas_catalog_version')} catalog please first run 'mas update' to switch to this catalog, this will ensure the appropriate actions are performed as part of the catalog update")
+            # else:
+            #     self.fatalError(f"IBM Maximo Operator Catalog is already installed on this cluster. However, it is not possible to identify its version. If you wish to install a new MAS instance using the {self.getParam('mas_catalog_version')} catalog please first run 'mas update' to switch to this catalog, this will ensure the appropriate actions are performed as part of the catalog update")
 
             if catalogId != self.getParam("mas_catalog_version"):
                 self.fatalError(f"IBM Maximo Operator Catalog {catalogId} is already installed on this cluster, if you wish to install a new MAS instance using the {self.getParam('mas_catalog_version')} catalog please first run 'mas update' to switch to this catalog, this will ensure the appropriate actions are performed as part of the catalog update")
