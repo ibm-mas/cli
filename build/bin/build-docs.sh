27f1dcbab21bb4a0fe74484e3f6189fd4163f5e1
Anil Prajapati <Anil.Prajapati@ibm.com>
2024-08-27 15:26:12 +0530

    [patch] update catalog docs and build-docs.sh

--- build/bin/build-docs.sh
+++ build/bin/build-docs.sh
@@ -3,11 +3,11 @@
 # Update all the placeholders in the doc source
 # Make sure not to commit these changes if you run this script locally
 find docs -type f -name '*.md' -exec sed -i \
-  -e 's/@@CLI_LATEST_VERSION@@/10.5.0/g' \
-  -e 's/@@MAS_PREVIOUS_CHANNEL@@/8.10.x/g' \
-  -e 's/@@MAS_PREVIOUS_CATALOG@@/v8-240625-amd64/g' \
+  -e 's/@@CLI_LATEST_VERSION@@/10.9.0/g' \
+  -e 's/@@MAS_PREVIOUS_CHANNEL@@/8.11.x/g' \
+  -e 's/@@MAS_PREVIOUS_CATALOG@@/v8-240730-amd64/g' \
   -e 's/@@MAS_LATEST_CHANNEL@@/9.0.x/g' \
-  -e 's/@@MAS_LATEST_CATALOG@@/v9-240730-amd64/g' \
+  -e 's/@@MAS_LATEST_CATALOG@@/v9-240827-amd64/g' \
   {} \;
 
 python -m pip install -q mkdocs mkdocs-redirects
