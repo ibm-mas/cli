29e4f5a675752d2aacbc0aad519b5b8320626adb
shajeena <shajsyed@in.ibm.com>
2024-08-30 20:46:19 +0530

    [minor]s390x

--- build/bin/docker-build.sh
+++ build/bin/docker-build.sh
@@ -26,6 +26,10 @@ do
         DOCKERFILE="$2"
         ;;
 
+        --target-platform)
+         TARGET_PLATFORM="$2"
+         ;;
+
         *)
         # unknown option, use as additional params directly to docker
         EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
@@ -36,7 +40,12 @@ do
 done
 
 BUILDPATH="${BUILDPATH:-image/$IMAGE}"
-DOCKERFILE="${DOCKERFILE:-$BUILDPATH/Dockerfile}"
+DOCKERFILE="${DOCKERFILE:-$BUILDPATH/Dockerfile.$TARGET_PLATFORM}"
+
+# Fallback to $BUILDPATH/Dockerfile if $DOCKERFILE does not exist
+if [ ! -e $DOCKERFILE ]; then
+  DOCKERFILE="$BUILDPATH/Dockerfile"
+fi
 
 # Note: Travis will ******* out the sensitive information here, but it will prove that
 # the value matches the value in the config if it does, which is the goal!
@@ -48,10 +57,33 @@ echo "VERSION_LABEL .. $DOCKER_TAG"
 echo "RELEASE_LABEL .. $GITHUB_RUN_ID"
 echo "VCS_REF ........ $GITHUB_SHA"
 echo "VCS_URL ........ https://github.com/$GITHUB_REPOSITORY"
-
+echo $TARGET_PLATFORM
+if [[ "$TARGET_PLATFORM" != "" ]] && [[ "$TARGET_PLATFORM" != "amd64" ]]; then
+  install_buildx
+fi
+if [[ "$TARGET_PLATFORM" == "" ]] || [[ "$TARGET_PLATFORM" == "amd64" ]]; then
+  if [[ "$TARGET_PLATFORM" == "amd64" ]]
+   then LOCAL_TAG=$NAMESPACE/$IMAGE:$DOCKER_TAG-$TARGET_PLATFORM
+  else LOCAL_TAG=$NAMESPACE/$IMAGE:$DOCKER_TAG
+  fi
 docker build \
+  --build-arg ARCHITECTURE=amd64 \
   --build-arg VERSION_LABEL=$DOCKER_TAG \
   --build-arg RELEASE_LABEL=$GITHUB_RUN_ID \
   --build-arg VCS_REF=$GITHUB_SHA \
   --build-arg VCS_URL=https://github.com/$GITHUB_REPOSITORY \
-  -t $NAMESPACE/$IMAGE $EXTRA_PARAMS -f $DOCKERFILE $BUILDPATH
+   -t $LOCAL_TAG $EXTRA_PARAMS -f $DOCKERFILE $BUILDPATH
+else
+  LOCAL_TAG=$NAMESPACE/$IMAGE:$DOCKER_TAG-$TARGET_PLATFORM
+  echo_highlight "Running multi-architecture build using docker buildx >>>"
+  docker buildx build --progress plain \
+    --load \
+    --platform linux/$TARGET_PLATFORM \
+    --build-arg ARCHITECTURE=$TARGET_PLATFORM \
+  --build-arg VERSION_LABEL=$DOCKER_TAG \
+  --build-arg RELEASE_LABEL=$GITHUB_RUN_ID \
+  --build-arg VCS_REF=$GITHUB_SHA \
+  --build-arg VCS_URL=https://github.com/$GITHUB_REPOSITORY \
+    -t $LOCAL_TAG $EXTRA_PARAMS -f $DOCKERFILE $BUILDPATH || exit 1
+
+fi
