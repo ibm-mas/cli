5049e57fb55406373503c110a3b76e8cca2409e2
shajeena <shajsyed@in.ibm.com>
2024-09-12 19:54:30 +0530

    [minor]Userimput for architecture

--- python/src/mas/cli/install/app.py
+++ python/src/mas/cli/install/app.py
@@ -146,8 +146,8 @@ class InstallApp(BaseApp, InstallSettingsMixin, InstallSummarizerMixin, ConfigGe
         else:
             print(tabulate(installoption, headers="keys", tablefmt="simple_grid"))
             catalogSelection = self.promptForInt("Select catalog and release", default=1)
-            self.setParam("mas_catalog_version", self.installOptions[catalogSelection-1]["catalog"])
-            self.setParam("mas_channel", self.installOptions[catalogSelection-1]["release"])
+            self.setParam("mas_catalog_version", installOption[catalogSelection-1]["catalog"])
+            self.setParam("mas_channel", installOption[catalogSelection-1]["release"])
 
     def configSLS(self) -> None:
         self.printH1("Configure Product License")
