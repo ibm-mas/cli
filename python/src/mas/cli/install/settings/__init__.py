fe97a906429999b3f4646e631ba761ff0fa6cdaf
shajeena <shajsyed@in.ibm.com>
2024-09-12 23:14:02 +0530

    [minor]Userimput for architecture

--- python/src/mas/cli/install/settings/__init__.py
+++ python/src/mas/cli/install/settings/__init__.py
@@ -9,13 +9,13 @@
 # *****************************************************************************
 
 from .db2Settings import Db2SettingsMixin
+from .mongodbSettings import MongodbSettingsMixin
 from .kafkaSettings import KafkaSettingsMixin
 from .manageSettings import ManageSettingsMixin
 from .turbonomicSettings import TurbonomicSettingsMixin
 from .additionalConfigs import AdditionalConfigsMixin
-from .MongodbSettings import MongodbSettingsMixin
 
-class InstallSettingsMixin(Db2SettingsMixin, KafkaSettingsMixin, ManageSettingsMixin, TurbonomicSettingsMixin, AdditionalConfigsMixin,MongodbSettingsMixin):
+class InstallSettingsMixin(Db2SettingsMixin, MongodbSettingsMixin,KafkaSettingsMixin, ManageSettingsMixin, TurbonomicSettingsMixin, AdditionalConfigsMixin):
   """
   This class collects all the Mixins providing interactive prompts for mas-install
   """
