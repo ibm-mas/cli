6c1bb2eea929658f1decc80b4db834db16aa84d7
Paul Stone <STONEPD@uk.ibm.com>
2024-10-01 08:34:48 +0100

    [patch] support dev mode catalogs and cpd version in mas update

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -37,6 +37,7 @@ class UpdateApp(BaseApp):
         """
         self.args = updateArgParser.parse_args(args=argv)
         self.noConfirm = self.args.no_confirm
+        self.devMode = self.args.dev_mode
 
         if self.args.mas_catalog_version:
             # Non-interactive mode
@@ -52,7 +53,12 @@ class UpdateApp(BaseApp):
                 "kafka_provider",
                 "dro_migration",
                 "dro_storage_class",
-                "dro_namespace"
+                "dro_namespace",
+                "dev_mode",
+                "cpd_product_version",
+                # Dev Mode
+                "artifactory_username",
+                "artifactory_token"
             ]
             for key, value in vars(self.args).items():
                 # These fields we just pass straight through to the parameters and fail if they are not set
@@ -91,7 +97,8 @@ class UpdateApp(BaseApp):
             self.chooseCatalog()
 
         # Validations
-        self.validateCatalog()
+        if not self.devMode:
+            self.validateCatalog()
 
         self.printH1("Dependency Update Checks")
         with Halo(text='Checking for IBM Watson Discovery', spinner=self.spinner) as h:
@@ -333,13 +340,18 @@ class UpdateApp(BaseApp):
                     # the case bundles in there anymore
                     # Longer term we will centralise this information inside the mas-devops python collection,
                     # where it can be made available to both the ansible collection and this python package.
+                    defaultMongoVersion = "6.0.12"
                     mongoVersions = {
                         "v9-240625-amd64": "6.0.12",
                         "v9-240730-amd64": "6.0.12",
                         "v9-240827-amd64": "6.0.12"
                     }
+                    catalogVersion = self.getParam('mas_catalog_version')
+                    if catalogVersion in mongoVersions:
+                        targetMongoVersion = mongoVersions[self.getParam('mas_catalog_version')]
+                    else:
+                        targetMongoVersion = defaultMongoVersion
 
-                    targetMongoVersion = mongoVersions[self.getParam('mas_catalog_version')]
                     self.setParam("mongodb_version", targetMongoVersion)
 
                     targetMongoVersionMajor = targetMongoVersion.split(".")[0]
@@ -450,8 +462,7 @@ class UpdateApp(BaseApp):
         cp4dVersions = {
             "v9-240625-amd64": "4.8.0",
             "v9-240730-amd64": "4.8.0",
-            "v9-240827-amd64": "4.8.0"
-            
+            "v9-240827-amd64": "4.8.0" 
         }
 
         with Halo(text='Checking for IBM Cloud Pak for Data', spinner=self.spinner) as h:
@@ -472,7 +483,10 @@ class UpdateApp(BaseApp):
                 if len(cpds) > 0:
                     cpdInstanceNamespace = cpds[0]["metadata"]["namespace"]
                     cpdInstanceVersion = cpds[0]["spec"]["version"]
-                    cpdTargetVersion = cp4dVersions[self.getParam("mas_catalog_version")]
+                    if self.args.cpd_product_version:
+                        cpdTargetVersion = self.getParam("cpd_product_version")
+                    else:
+                        cpdTargetVersion = cp4dVersions[self.getParam("mas_catalog_version")]
 
                     currentCpdVersionMajorMinor = f"{cpdInstanceVersion.split('.')[0]}.{cpdInstanceVersion.split('.')[1]}"
                     targetCpdVersionMajorMinor = f"{cpdTargetVersion.split('.')[0]}.{cpdTargetVersion.split('.')[1]}"
@@ -488,7 +502,7 @@ class UpdateApp(BaseApp):
                                 "<u>Dependency Update Notice</u>",
                                 f"Cloud Pak For Data is currently running version {cpdInstanceVersion} and will be updated to version {cpdTargetVersion}",
                                 "It is recommended that you backup your Cloud Pak for Data instance before proceeding:",
-                                "  <u>https://www.ibm.com/docs/en/cloud-paks/cp-data/4.8.x?topic=administering-backing-up-restoring-cloud-pak-data</u>"
+                                "  <u>https://www.ibm.com/docs/en/cloud-paks/cp-data/5.0.x?topic=administering-backing-up-restoring-cloud-pak-data</u>"
                             ])
 
                         # Lookup the storage classes already used by CP4D
