cf75c934ca24cd6c6013fec41f0cbdd486710dd9
David Parker <parkerda@uk.ibm.com>
2024-10-31 15:01:03 +0000

    Support configurable SLS MongoDb path

---
+++ python/src/mas/cli/install/catalogs.py
@@ -0,0 +1,141 @@
+# *****************************************************************************
+# Copyright (c) 2024 IBM Corporation and other Contributors.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# *****************************************************************************
+
+catalogChoices = {
+    "amd64": [
+        {
+            "#": 1,
+            "catalog": "v9-241003-amd64",
+            "release": "9.0.x",
+            "core": "9.0.3",
+            "assist": "9.0.2",
+            "iot": "9.0.3",
+            "manage": "9.0.3",
+            "monitor": "9.0.3",
+            "optimizer": "9.0.3",
+            "predict": "9.0.2",
+            "inspection": "9.0.3",
+            "aibroker": "9.0.2"
+        },
+        {
+            "#": 2,
+            "catalog": "v9-241003-amd64",
+            "release": "8.11.x",
+            "core": "8.11.15",
+            "assist": "8.8.6",
+            "iot": "8.8.13",
+            "manage": "8.7.12",
+            "monitor": "8.11.11",
+            "optimizer": "8.5.9",
+            "predict": "8.9.5",
+            "inspection": "8.9.6"
+        },
+        {
+            "#": 3,
+            "catalog": "v9-241003-amd64",
+            "release": "8.10.x",
+            "core": "8.10.18",
+            "assist": "8.7.7",
+            "iot": "8.7.17",
+            "manage": "8.6.18",
+            "monitor": "8.10.14",
+            "optimizer": "8.4.10",
+            "predict": "8.8.3",
+            "inspection": "8.8.4"
+        },
+        {
+            "#": 4,
+            "catalog": "v9-240827-amd64",
+            "release": "9.0.x",
+            "core": "9.0.2",
+            "assist": "9.0.2",
+            "iot": "9.0.2",
+            "manage": "9.0.2",
+            "monitor": "9.0.2",
+            "optimizer": "9.0.2",
+            "predict": "9.0.1",
+            "inspection": "9.0.2"
+        },
+        {
+            "#": 5,
+            "catalog": "v9-240827-amd64",
+            "release": "8.11.x",
+            "core": "8.11.14",
+            "assist": "8.8.6",
+            "iot": "8.8.12",
+            "manage": "8.7.11",
+            "monitor": "8.11.10",
+            "optimizer": "8.5.8",
+            "predict": "8.9.3",
+            "inspection": "8.9.5"
+        },
+        {
+            "#": 6,
+            "catalog": "v9-240827-amd64",
+            "release": "8.10.x",
+            "core": "8.10.17",
+            "assist": "8.7.7",
+            "iot": "8.7.16",
+            "manage": "8.6.17",
+            "monitor": "8.10.13",
+            "optimizer": "8.4.9",
+            "predict": "8.8.3",
+            "inspection": "8.8.4"
+        },
+        {
+            "#": 7,
+            "catalog": "v9-240730-amd64",
+            "release": "9.0.x",
+            "core": "9.0.1",
+            "assist": "9.0.1",
+            "iot": "9.0.1",
+            "manage": "9.0.1",
+            "monitor": "9.0.1",
+            "optimizer": "9.0.1",
+            "predict": "9.0.0",
+            "inspection": "9.0.0"
+        },
+        {
+            "#": 8,
+            "catalog": "v9-240730-amd64",
+            "release": "8.11.x",
+            "core": "8.11.13",
+            "assist": "8.8.5",
+            "iot": "8.8.11",
+            "manage": "8.7.10",
+            "monitor": "8.11.9",
+            "optimizer": "8.5.7",
+            "predict": "8.9.3",
+            "inspection": "8.9.4"
+        },
+        {
+            "#": 9,
+            "catalog": "v9-240730-amd64",
+            "release": "8.10.x",
+            "core": "8.10.16",
+            "assist": "8.7.6",
+            "iot": "8.7.15",
+            "manage": "8.6.16",
+            "monitor": "8.10.12",
+            "optimizer": "8.4.8",
+            "predict": "8.8.3",
+            "inspection": "8.8.4"
+        }
+    ],
+    "s390x": [
+        {
+            "#": 1,
+            "catalog": "v9-241003-s390x",
+            "release": "9.0.x",
+            "core": "9.0.3",
+            "manage": "9.0.3"
+        }
+    ]
+}
