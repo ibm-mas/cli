123ac929410eabacfafa46ea2f0403909dca6745
parveenkumaribm <parveenkumar@ibm.com>
2024-11-07 13:40:30 +0530

    [patch] tar file and manage 9.0.3 update

--- python/src/mas/cli/install/catalogs.py
+++ python/src/mas/cli/install/catalogs.py
@@ -17,7 +17,7 @@ catalogChoices = {
             "core": "9.0.4",
             "assist": "9.0.2",
             "iot": "9.0.4",
-            "manage": "9.0.5",
+            "manage": "9.0.3",
             "monitor": "9.0.4",
             "optimizer": "9.0.4",
             "predict": "9.0.2",
