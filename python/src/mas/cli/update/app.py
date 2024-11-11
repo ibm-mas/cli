a0f272a9b62d13065d6067a526526ca4175125de
Anil Prajapati <Anil.Prajapati@ibm.com>
2024-11-12 02:58:22 +0530

    [patch] use cpd 4.8.0, fix indent

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -250,7 +250,7 @@ class UpdateApp(BaseApp):
         ])
 
         catalogOptions = [
-           "v9-241107-amd64", "v9-241003-amd64", "v9-240827-amd64"
+            "v9-241107-amd64", "v9-241003-amd64", "v9-240827-amd64"
         ]
         self.promptForListSelect("Select catalog version", catalogOptions, "mas_catalog_version", default=1)
 
@@ -475,7 +475,7 @@ class UpdateApp(BaseApp):
             "v9-240730-amd64": "4.8.0",
             "v9-240827-amd64": "4.8.0",
             "v9-241003-amd64": "4.8.0",
-            "v9-241107-amd64": "5.0.0"
+            "v9-241107-amd64": "4.8.0"
         }
 
         with Halo(text='Checking for IBM Cloud Pak for Data', spinner=self.spinner) as h:
