8fb9484f05d0c974d55c5e1e86c825429aafbf78
Rawa Resul <rawa.resul@ibm.com>
2024-11-13 17:05:06 +0000

    [patch] Further catalog updates

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -244,13 +244,14 @@ class UpdateApp(BaseApp):
         self.printH1("Select IBM Maximo Operator Catalog Version")
         self.printDescription([
             "Select MAS Catalog",
-            "  1) Nov 07 2024 Update (MAS 9.0.5, 8.11.16, &amp; 8.10.19)",
-            "  1) Oct 03 2024 Update (MAS 9.0.3, 8.11.15, &amp; 8.10.18)",
-            "  2) Aug 27 2024 Update (MAS 9.0.2, 8.11.14, &amp; 8.10.17)",
+            "  1) Sep 20 2024 Update (MAS 9.1.0-feature, 8.11.16, &amp; 8.10.19)",
+            "  2) Nov 07 2024 Update (MAS 9.0.5, 8.11.16, &amp; 8.10.19)",
+            "  3) Oct 03 2024 Update (MAS 9.0.3, 8.11.15, &amp; 8.10.18)",
+            "  4) Aug 27 2024 Update (MAS 9.0.2, 8.11.14, &amp; 8.10.17)",
         ])
 
         catalogOptions = [
-            "v9-241107-amd64", "v9-241003-amd64", "v9-240827-amd64"
+           "v9-240920-amd64", "v9-241107-amd64", "v9-241003-amd64", "v9-240827-amd64"
         ]
         self.promptForListSelect("Select catalog version", catalogOptions, "mas_catalog_version", default=1)
 
@@ -347,7 +348,8 @@ class UpdateApp(BaseApp):
                         "v9-240730-amd64": "6.0.12",
                         "v9-240827-amd64": "6.0.12",
                         "v9-241003-amd64": "6.0.12",
-                        "v9-241107-amd64": "7.0.12"
+                        "v9-241107-amd64": "7.0.12",
+                        "v9-240920-amd64": "7.0.12"
                     }
                     catalogVersion = self.getParam('mas_catalog_version')
                     if catalogVersion in mongoVersions:
