0f2ac1ab5349121bfc0403bdd41d28c9569d0e12
Rawa Resul <rawa.resul@ibm.com>
2024-12-05 17:46:06 +0000

    [patch] revert changes

--- python/setup.py
+++ python/setup.py
@@ -58,7 +58,7 @@ setup(
     description='Python Admin CLI for Maximo Application Suite',
     long_description=long_description,
     install_requires=[
-        'mas-devops == 1.11.2-pre.MASR-2387',  # EPL
+        'mas-devops >= 1.10.0',  # EPL
         'halo',                  # MIT License
         'prompt_toolkit',        # BSD License
         'openshift',             # Apache Software License
