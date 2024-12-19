15b36eaa9091b51b5b3137f13f548cff0b729fc8
Leonardo Miranda <Leo.Miranda@ibm.com>
2024-12-19 14:34:20 -0300

    [patch] manage attachment provider choices

--- python/src/mas/cli/install/argParser.py
+++ python/src/mas/cli/install/argParser.py
@@ -664,7 +664,8 @@ manageArgGroup.add_argument(
     "--manage-attachments-provider",
     dest="mas_manage_attachments_provider",
     required=False,
-    help="Defines the storage provider type to be used to store attachments in Maximo Manage"
+    help="Defines the storage provider type to be used to store attachments in Maximo Manage",
+    choices=["filestorage", "ibm", "aws"]
 )
 manageArgGroup.add_argument(
     "--manage-attachments-mode",
