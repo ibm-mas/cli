7ead3951975a9ea060dae714eece3a5d71fcf870
Anil Prajapati <Anil.Prajapati@ibm.com>
2025-02-18 12:48:43 +0530

    [minor] march release prep

--- build/bin/build-docs.sh
+++ build/bin/build-docs.sh
@@ -5,10 +5,10 @@
 find docs -type f -name '*.md' -exec sed -i \
   -e 's/@@CLI_LATEST_VERSION@@/13.3.0/g' \
   -e 's/@@MAS_PREVIOUS_CHANNEL@@/8.11.x/g' \
-  -e 's/@@MAS_PREVIOUS_CATALOG@@/v9-250109-amd64/g' \
+  -e 's/@@MAS_PREVIOUS_CATALOG@@/v9-250206-amd64/g' \
   -e 's/@@MAS_LATEST_CHANNEL@@/9.0.x/g' \
   -e 's/@@MAS_LATEST_CHANNEL_MANAGE@@/9.0.x/g' \
-  -e 's/@@MAS_LATEST_CATALOG@@/v9-250206-amd64/g' \
+  -e 's/@@MAS_LATEST_CATALOG@@/v9-250306-amd64/g' \
   {} \;
 
 python -m pip install -q mkdocs mkdocs-carbon mkdocs-glightbox mkdocs-redirects
