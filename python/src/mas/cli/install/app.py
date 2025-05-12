c79c60eebeee0cea2fc9ba4a0f3170246e193038
shajeena <shajsyed@in.ibm.com>
2025-05-12 13:23:33 +0530

    [patch] Rebase support for ppc64le

--- python/src/mas/cli/install/app.py
+++ python/src/mas/cli/install/app.py
@@ -327,7 +327,7 @@ class InstallApp(BaseApp, InstallSettingsMixin, InstallSummarizerMixin, ConfigGe
     @logMethodCall
     def configGrafana(self) -> None:
         if self.architecture == "s390x" or self.architecture == "ppc64le":
-            # We are not supporting Grafana on s390x at the moment
+            # We are not supporting Grafana on s390x /ppc64le at the moment
             self.setParam("grafana_action", "none")
         else:
             try:
