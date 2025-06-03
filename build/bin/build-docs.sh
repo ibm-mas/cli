aec1b7e1551a994d777fd460a875fcc284e5f50b
Anil Prajapati <Anil.Prajapati@ibm.com>
2025-06-03 22:28:35 +0530

    [minor] add 90 250605 patch

--- build/bin/build-docs.sh
+++ build/bin/build-docs.sh
@@ -5,10 +5,10 @@
 find docs -type f -name '*.md' -exec sed -i \
   -e 's/@@CLI_LATEST_VERSION@@/13.20.0/g' \
   -e 's/@@MAS_PREVIOUS_CHANNEL@@/8.11.x/g' \
-  -e 's/@@MAS_PREVIOUS_CATALOG@@/v9-250403-amd64/g' \
+  -e 's/@@MAS_PREVIOUS_CATALOG@@/v9-250501-amd64/g' \
   -e 's/@@MAS_LATEST_CHANNEL@@/9.0.x/g' \
   -e 's/@@MAS_LATEST_CHANNEL_MANAGE@@/9.0.x/g' \
-  -e 's/@@MAS_LATEST_CATALOG@@/v9-250501-amd64/g' \
+  -e 's/@@MAS_LATEST_CATALOG@@/v9-250605-amd64/g' \
   {} \;
 
 python -m pip install -q mkdocs mkdocs-carbon mkdocs-glightbox mkdocs-redirects
