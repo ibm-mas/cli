bf3a90ccdff4f9045c2da1f5f4ccee10d68e2d0d
Harsh Tamakuwala <Harsh.Tamakuwala@ibm.com>
2025-06-20 00:37:21 +0530

    [patch] Update offline upgrade logic

--- image/cli/app-root/src/start-offline-upgrade.sh
+++ image/cli/app-root/src/start-offline-upgrade.sh
@@ -26,48 +26,49 @@ if [[ -n $MANAGE_WORKSPACE_NAME ]]; then
 		PATCH_DONE=false
 
 		# Check if online upgrade is completed and workspace is ready for offline upgrade
-		while [[ $MANAGE_DEPLOYMENTREADY_MSG == *"$ONLINE_UPDATE_DONE_MSG"* && $RETRIES_USED -le $MAX_RETRIES ]]
+		while [[ $MANAGE_DEPLOYMENTREADY_MSG != *"$ONLINE_UPDATE_DONE_MSG"* && $RETRIES_USED -le $MAX_RETRIES ]]
 		do
-			if [[ $MANAGE_DEPLOYMENTREADY_MSG == *"$ONLINE_UPDATE_DONE_MSG"* ]]; then
-				echo "Status found is: ${MANAGE_DEPLOYMENTREADY_MSG}"
-				echo "ManageWorkspace indicates it is ready for offline upgrade, patching manageofflineupgraderequest CR to requested, and removing old status if present"
-				UPGRADE_REQUEST_STATUS=$(oc get -n ${MAS_APP_NAMESPACE} manageofflineupgraderequest.apps.mas.ibm.com -l mas.ibm.com/instanceId=${MAS_INSTANCE_ID} -o=jsonpath="{.status}")
-				if [[ -n $UPGRADE_REQUEST_STATUS ]]; then 
-						oc patch -n ${MAS_APP_NAMESPACE} manageofflineupgraderequest.apps.mas.ibm.com -l mas.ibm.com/instanceId=${MAS_INSTANCE_ID} --subresource status --type=json -p="[{'op': 'remove', 'path': '/status'}]"
-				fi
-				oc patch -n ${MAS_APP_NAMESPACE} manageofflineupgraderequests.apps.mas.ibm.com -l mas.ibm.com/instanceId=${MAS_INSTANCE_ID} --type merge -p $'spec:\n stage: requested'
-				echo "Patch complete for manageofflineupgraderequest CR"
-				PATCH_DONE=true
-			else
-				echo "[${RETRIES_USED}/${MAX_RETRIES}] ManageWorkspace status does not indicate it is ready for offlineupgrade. Waiting ${DElAY} seconds before checking again."
-				sleep 120
-				RETRIES_USED=$((RETRIES_USED + 1))
-				MANAGE_DEPLOYMENTREADY_MSG=$(oc get -n ${MAS_APP_NAMESPACE} ${MANAGE_WORKSPACE_NAME} -o=jsonpath="{.status.conditions[?(@.type=='DeploymentReady')].message}")
-			fi
+			echo "[${RETRIES_USED}/${MAX_RETRIES}] ManageWorkspace status does not indicate it is ready for offlineupgrade. Waiting ${DELAY} seconds before checking again."
+			sleep 120
+			RETRIES_USED=$((RETRIES_USED + 1))
+			MANAGE_DEPLOYMENTREADY_MSG=$(oc get -n ${MAS_APP_NAMESPACE} ${MANAGE_WORKSPACE_NAME} -o=jsonpath="{.status.conditions[?(@.type=='DeploymentReady')].message}")
 		done
 		
 		# Check the status of offline upgrade if the patch is completed
-		if [[ $PATCH_DONE = false ]]; then
-			echo "The ManageWorkspace Status does not indicate it is ready for offlineupgrade even after ${MAX_RETRIES} retries (${DELAY}s delay), so do nothing"
-			echo "Final Status found is: ${MANAGE_DEPLOYMENTREADY_MSG}"
-		else
+		if [[ $MANAGE_DEPLOYMENTREADY_MSG == *"$ONLINE_UPDATE_DONE_MSG"* ]]; then
+			echo "Status found is: ${MANAGE_DEPLOYMENTREADY_MSG}"
+			echo "ManageWorkspace indicates it is ready for offline upgrade, patching manageofflineupgraderequest CR to requested, and removing old status if present"
+			UPGRADE_REQUEST_STATUS=$(oc get -n ${MAS_APP_NAMESPACE} manageofflineupgraderequest.apps.mas.ibm.com -l mas.ibm.com/instanceId=${MAS_INSTANCE_ID} -o=jsonpath="{.status}")
+			UPGRADE_REQUEST_NAME=$(oc get ManageOfflineUpgradeRequest -n ${MAS_APP_NAMESPACE} -l mas.ibm.com/instanceId=${MAS_INSTANCE_ID} -o name)
+			if [[ -n $UPGRADE_REQUEST_STATUS ]]; then 
+				oc patch -n ${MAS_APP_NAMESPACE} ${UPGRADE_REQUEST_NAME} --subresource status --type=json -p="[{'op': 'remove', 'path': '/status'}]"
+			fi
+			oc patch -n ${MAS_APP_NAMESPACE} ${UPGRADE_REQUEST_NAME} --type merge -p $'spec:\n stage: requested'
+			echo "Patch complete for manageofflineupgraderequest CR"
+
 			RETRIES_USED=1
 			MAX_RETRIES=45
 			DELAY=120
 			READY_STATUS=$(oc get -n ${MAS_APP_NAMESPACE} ${MANAGE_WORKSPACE_NAME} -o=jsonpath="{.status.conditions[?(@.type=='Ready')].reason}")
-			while [[ $READY_STATUS == *"Ready"* && $RETRIES_USED -le $MAX_RETRIES ]]
+			while [[ $READY_STATUS != *"Ready"* && $RETRIES_USED -le $MAX_RETRIES ]]
 			do
-				if [[ $READY_STATUS == *"Ready"* ]]; then
-					echo "Status found is: ${READY_STATUS}"
-					echo "Online and Offline upgrade complete. Server bundles are up and running."
-					exit 0
-				else
-					echo "[${RETRIES_USED}/${MAX_RETRIES}] ManageWorkspace status does not indicate it is ready for offlineupgrade. Waiting ${DElAY} seconds before checking again."
-					sleep 120
-					RETRIES_USED=$((RETRIES_USED + 1))
-					READY_STATUS=$(oc get -n ${MAS_APP_NAMESPACE} ${MANAGE_WORKSPACE_NAME} -o=jsonpath="{.status.conditions[?(@.type=='Ready')].reason}")
-				fi
+				echo "[${RETRIES_USED}/${MAX_RETRIES}] ManageWorkspace is not ready yet. Waiting ${DELAY} seconds before checking again."
+				sleep 120
+				RETRIES_USED=$((RETRIES_USED + 1))
+				READY_STATUS=$(oc get -n ${MAS_APP_NAMESPACE} ${MANAGE_WORKSPACE_NAME} -o=jsonpath="{.status.conditions[?(@.type=='Ready')].reason}")
 			done
+
+			if [[ $READY_STATUS == *"Ready"* ]]; then
+				echo "Status found is: ${READY_STATUS}"
+				echo "Online and Offline upgrade complete. Server bundles are up and running."
+				exit 0
+			else
+				echo "[FAILED]: ManageWorkspace did not become ready even after ${MAX_RETRIES} retries (${DELAY}s delay)"
+				exit 1
+			fi
+		else
+			echo "The ManageWorkspace Status does not indicate it is ready for offlineupgrade even after ${MAX_RETRIES} retries (${DELAY}s delay), so do nothing"
+			echo "Final Status found is: ${MANAGE_DEPLOYMENTREADY_MSG}"
 		fi
 	else
 		echo "Upgrade type is: ${MANAGE_UPGRADE_TYPE}."
