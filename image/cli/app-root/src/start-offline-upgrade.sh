c4d210720cd73b5fbeb3ee05d071b3e1fe16b58d
Harsh Tamakuwala <Harsh.Tamakuwala@ibm.com>
2025-06-20 18:59:09 +0530

    [patch] Update logging

--- image/cli/app-root/src/start-offline-upgrade.sh
+++ image/cli/app-root/src/start-offline-upgrade.sh
@@ -36,6 +36,7 @@ if [[ -n $MANAGE_WORKSPACE_NAME ]]; then
 		
 		# Check the status of offline upgrade if the patch is completed
 		if [[ $MANAGE_DEPLOYMENTREADY_MSG == *"$ONLINE_UPDATE_DONE_MSG"* ]]; then
+			echo
 			echo "Status found is: ${MANAGE_DEPLOYMENTREADY_MSG}"
 			echo "ManageWorkspace indicates it is ready for offline upgrade, patching manageofflineupgraderequest CR to requested, and removing old status if present"
 			UPGRADE_REQUEST_STATUS=$(oc get -n ${MAS_APP_NAMESPACE} manageofflineupgraderequest.apps.mas.ibm.com -l mas.ibm.com/instanceId=${MAS_INSTANCE_ID} -o=jsonpath="{.status}")
@@ -45,6 +46,7 @@ if [[ -n $MANAGE_WORKSPACE_NAME ]]; then
 			fi
 			oc patch -n ${MAS_APP_NAMESPACE} ${UPGRADE_REQUEST_NAME} --type merge -p $'spec:\n stage: requested'
 			echo "Patch complete for manageofflineupgraderequest CR"
+			echo
 
 			RETRIES_USED=1
 			MAX_RETRIES=45
@@ -59,21 +61,26 @@ if [[ -n $MANAGE_WORKSPACE_NAME ]]; then
 			done
 
 			if [[ $READY_STATUS == *"Ready"* ]]; then
+				echo
 				echo "Status found is: ${READY_STATUS}"
 				echo "Online and Offline upgrade complete. Server bundles are up and running."
 				exit 0
 			else
+				echo
 				echo "[FAILED]: ManageWorkspace did not become ready even after ${MAX_RETRIES} retries (${DELAY}s delay)"
 				exit 1
 			fi
 		else
+			echo
 			echo "The ManageWorkspace Status does not indicate it is ready for offlineupgrade even after ${MAX_RETRIES} retries (${DELAY}s delay), so do nothing"
 			echo "Final Status found is: ${MANAGE_DEPLOYMENTREADY_MSG}"
 		fi
 	else
+		echo
 		echo "Upgrade type is: ${MANAGE_UPGRADE_TYPE}."
 		echo "Upgrade type for manage is not onlineUpgrade, so skipping the step to patch manageofflineupgraderequest CR."
 	fi
 else
+	echo
 	echo "ManageWorkspace not found with instance id: ${MAS_INSTANCE_ID} in namespace: ${MAS_APP_NAMESPACE}, so skipping the step to patch manageofflineupgraderequest CR."
 fi
