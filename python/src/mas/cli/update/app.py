d5fa44cbb489c25f29c7ad97e1db40ea4943ea70
Anil Prajapati <Anil.Prajapati@ibm.com>
2025-06-23 16:35:25 +0530

    [patch] replace 9.0.x with 9.1.x

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -242,7 +242,7 @@ class UpdateApp(BaseApp):
         self.printH1("Select IBM Maximo Operator Catalog Version")
         self.printDescription([
             "Select MAS Catalog",
-            "  1) Jun 05 2025 Update (MAS 9.0.12, 8.11.21, &amp; 8.10.26)",
+            "  1) Jun 05 2025 Update (MAS 9.1.0, 9.0.12, 8.11.21, &amp; 8.10.26)",
             "  2) May 01 2025 Update (MAS 9.0.11, 8.11.22, &amp; 8.10.25)",
             "  3) Apr 03 2025 Update (MAS 9.0.10, 8.11.21, &amp; 8.10.24)",
         ])
