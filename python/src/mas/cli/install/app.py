0fea80f7fd59041afbc245299101f70766bbc88d
Ian Boden <ianboden@uk.ibm.com>
2025-06-23 17:10:09 +0100

    Set default cpd if not using a known catalog

--- python/src/mas/cli/install/app.py
+++ python/src/mas/cli/install/app.py
@@ -376,7 +376,7 @@ class InstallApp(BaseApp, InstallSettingsMixin, InstallSummarizerMixin, ConfigGe
             self.printDescription([
                 f"Unknown catalog {self.getParam('mas_catalog_version')}, please manually select the version of Cloud Pak for Data to use"
             ])
-            self.promptForString("Cloud Pak for Data product version", "cpd_product_version", default="4.8.0")
+            self.promptForString("Cloud Pak for Data product version", "cpd_product_version", default="5.1.3")
             logger.debug(f"Using user-provided (prompt) CP4D product version: {self.getParam('cpd_product_version')}")
         else:
             logger.debug(f"Using user-provided (flags) CP4D product version: {self.getParam('cpd_product_version')}")
