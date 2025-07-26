c1c7d471e3ae9a7433619c6d3d134ceeb06e6663
jainyjoseph <jainy587@gmail.com>
2025-07-26 17:21:56 +0530

    [patch] Changes to test new variable for mongo

--- python/src/mas/cli/install/argBuilder.py
+++ python/src/mas/cli/install/argBuilder.py
@@ -143,7 +143,7 @@ class installArgBuilderMixin():
         if self.getParam('dro_namespace') != "":
             command += f"  --dro-namespace \"{self.getParam('dro_namespace')}\"{newline}"
 
-        # MongoDb Community Operator
+        # MongoDb Community Operator (DBversion variable)
         # -----------------------------------------------------------------------------
         if self.getParam('mongodb_namespace') != "":
             command += f"  --mongodb-namespace \"{self.getParam('mongodb_namespace')}\"{newline}"
