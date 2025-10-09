a7a662a703d5ac3b88bdec3abb527911dc8abf27
KAROL CZARNECKI <karol.czarnecki@ie.ibm.com>
2025-10-09 13:53:47 +0100

    [patch] added back aiservice_instance_id which was removed by mistake from params.py

--- python/src/mas/cli/aiservice/install/params.py
+++ python/src/mas/cli/aiservice/install/params.py
@@ -80,7 +80,7 @@ optionalParams = [
     "aiservice_watsonxai_instance_id",
     "aiservice_watsonxai_username",
     "aiservice_watsonxai_version",
-
+    "aiservice_instance_id",
     "minio_root_user",
     "minio_root_password",
 
