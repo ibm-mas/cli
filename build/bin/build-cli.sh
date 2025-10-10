a8d539f68e015cfc838e7d028274b5b3b6572387
Hari Palleti <Hari.Palleti@ibm.com>
2025-10-09 22:12:27 -0500

    [patch] artifactory url

--- build/bin/build-cli.sh
+++ build/bin/build-cli.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export ARTIFACTORY_GENERIC_RELEASE_URL=${ARTIFACTORY_GENERIC_RELEASE_URL:-https://na.artifactory.swg-devops.com/artifactory/wiotp-generic-release}
+
 if [ "$DEV_MODE" != "true" ]; then
   source ${GITHUB_WORKSPACE}/build/bin/.env.sh
   source ${GITHUB_WORKSPACE}/build/bin/.functions.sh
