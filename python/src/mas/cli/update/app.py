29160cf9d46a069994fbe3fbe48c255ed90a963a
Parveen Kumar <Parveenkumar@ibm.com>
2025-10-17 13:51:39 +0530

    [patch] fix syntax error

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -260,7 +260,7 @@ class UpdateApp(BaseApp):
         ])
 
         catalogOptions = [
-            "v9-251030-amd64","v9-251010-amd64", "v9-250925-amd64",
+            "v9-251030-amd64", "v9-251010-amd64", "v9-250925-amd64",
         ]
         self.promptForListSelect("Select catalog version", catalogOptions, "mas_catalog_version", default=1)
 
