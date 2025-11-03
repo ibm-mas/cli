16ea05cb7221756e948c3549e24c89e87fc5ff56
David Parker <parkerda@uk.ibm.com>
2025-11-03 16:48:35 +0000

    Update install-ansible-collections.sh

--- image/cli/install/install-ansible-collections.sh
+++ image/cli/install/install-ansible-collections.sh
@@ -15,11 +15,12 @@ if [[ -e /tmp/install/ibm-mas_devops.tar.gz ]]; then
     echo "Installing local build of ansible-devops from archive"
     ansible-galaxy collection install /tmp/install/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
 elif [[ "$GITHUB_REF_NAME" != "master" ]] && [[ "$GITHUB_REF_TYPE" == "branch" ]]; then
-    BRANCH_TARGET_URL="${ARTIFACTORY_GENERIC_RELEASE_URL}/ansible-devops/branches/ibm-mas_devops-${GITHUB_REF_NAME}.tar.gz"
+    # Download and validate the archive
+    BRANCH_TARGET_URL="${ARTIFACTORY_GENERIC_RELEASE_URL}/ibm-mas/ansible-devops/branches/ibm-mas_devops-${GITHUB_REF_NAME}.tar.gz"
     curl -H "Authorization:Bearer $ARTIFACTORY_TOKEN" $BRANCH_TARGET_URL -o /tmp/install/ibm-mas_devops.tar.gz
+    tar -tzf /tmp/install/ibm-mas_devops.tar.gz
     if [[ "$?" == "0" ]]; then
         echo "Installing matching branch build of ansible-devops from Artifactory"
-        tar -tzf /tmp/install/ibm-mas_devops.tar.gz
         ansible-galaxy collection install /tmp/install/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
     else
         echo "Installing release build of ansible-devops from Galaxy (branch build)"
