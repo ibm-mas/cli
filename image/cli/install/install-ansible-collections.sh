26c2c8914f71abcd94fed0067c4cd74368343cee
David Parker <parkerda@uk.ibm.com>
2025-11-03 09:28:42 +0000

    Update install-ansible-collections.sh

--- image/cli/install/install-ansible-collections.sh
+++ image/cli/install/install-ansible-collections.sh
@@ -6,7 +6,7 @@
 if [[ -e /tmp/install/ibm-mas_devops.tar.gz ]]; then
     echo "Installing local build of ansible-devops from archive"
     ansible-galaxy collection install /tmp/install/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
-elif [[ "$GITHUB_REF_NAME" != "master" ]] && [[ "$GITHUB_REF_TYPE" == "branch" ]]
+elif [[ "$GITHUB_REF_NAME" != "master" ]] && [[ "$GITHUB_REF_TYPE" == "branch" ]]; then
     BRANCH_TARGET_URL="${ARTIFACTORY_GENERIC_RELEASE_URL}/ansible-devops/branches/ibm-mas_devops-${GITHUB_REF_NAME}.tar.gz"
     curl -H "Authorization:Bearer $ARTIFACTORY_TOKEN" $BRANCH_TARGET_URL -o /tmp/install/ibm-mas_devops.tar.gz
     if [[ "$?" == "0" ]]; then
