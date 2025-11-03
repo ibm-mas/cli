34c38c5061ad2eaab816dc6204a447e25960d761
David Parker <parkerda@uk.ibm.com>
2025-11-03 09:02:50 +0000

    [minor] Add Turbonomic integration to OCP provision functions

--- image/cli/install/install-ansible-collections.sh
+++ image/cli/install/install-ansible-collections.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-if [[ -e /tmp/install/ibm-mas_devops.tar.gz ]]
-then ansible-galaxy collection install /tmp/install/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
-else ansible-galaxy collection install ibm.mas_devops
+# If the local file is present, defer to this
+# Otherwise, check for a matching branch name in Artifactory
+# Otherwise, install the most recent version from Galaxy
+if [[ -e /tmp/install/ibm-mas_devops.tar.gz ]]; then
+    echo "Installing local build of ansible-devops from archive"
+    ansible-galaxy collection install /tmp/install/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
+elif [[ "$GITHUB_REF_NAME" != "master" ]] && [[ "$GITHUB_REF_TYPE" == "branch" ]]
+    BRANCH_TARGET_URL="${ARTIFACTORY_GENERIC_RELEASE_URL}/ansible-devops/branches/ibm-mas_devops-${GITHUB_REF_NAME}.tar.gz"
+    curl -H "Authorization:Bearer $ARTIFACTORY_TOKEN" $BRANCH_TARGET_URL -o /tmp/install/ibm-mas_devops.tar.gz
+    if [[ "$?" == "0" ]]; then
+        echo "Installing matching branch build of ansible-devops from Artifactory"
+        ansible-galaxy collection install /tmp/install/ibm-mas_devops.tar.gz -p $ANSIBLE_COLLECTIONS_PATH
+    else
+        echo "Installing release build of ansible-devops from Galaxy"
+        ansible-galaxy collection install ibm.mas_devops
+    fi
+else
+    echo "Installing release build of ansible-devops from Galaxy"
+    ansible-galaxy collection install ibm.mas_devops
 fi
