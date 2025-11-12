1af058b621beac424e7e1ced040875f2789710ce
Parveen Kumar <Parveenkumar@ibm.com>
2025-11-12 18:35:44 +0530

    [patch] corrected intention

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -264,7 +264,7 @@ class UpdateApp(BaseApp):
         ])
 
         catalogOptions = [
-             "v9-251127-amd64", "v9-251030-amd64", "v9-251010-amd64",
+            "v9-251127-amd64", "v9-251030-amd64", "v9-251010-amd64",
         ]
         self.promptForListSelect("Select catalog version", catalogOptions, "mas_catalog_version", default=1)
 
