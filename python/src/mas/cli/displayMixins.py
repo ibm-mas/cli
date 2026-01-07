e11f7843423cf182c1f463962fa8358324798cd4
David Parker <parkerda@uk.ibm.com>
2026-01-07 11:19:24 +0000

    [patch] First phase of adding type hints

--- python/src/mas/cli/displayMixins.py
+++ python/src/mas/cli/displayMixins.py
@@ -12,6 +12,7 @@ from os import getenv
 from prompt_toolkit import prompt, print_formatted_text, HTML, PromptSession
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.validation import Validator
+from typing import List
 
 from .validators import YesNoValidator, IntValidator, FileExistsValidator, DirectoryExistsValidator
 
@@ -27,66 +28,66 @@ PROMPTCOLOR = "Yellow"
 
 
 class PrintMixin():
-    def printTitle(self, message):
-        print_formatted_text(HTML(f"<b><u>{message.replace(' & ', ' &amp; ')}</u></b>"))
+    def printTitle(self, message: str) -> None:
+        print_formatted_text(HTML(f"<b><u>{message.replace(' & ', ' & ')}</u></b>"))
 
-    def printH1(self, message):
-        self.h1count += 1
-        self.h2count = 0
+    def printH1(self, message: str) -> None:
+        self.h1count += 1  # type: ignore
+        self.h2count = 0  # type: ignore
         print()
-        print_formatted_text(HTML(f"<u><{H1COLOR}>{self.h1count}) {message.replace(' & ', ' &amp; ')}</{H1COLOR}></u>"))
+        print_formatted_text(HTML(f"<u><{H1COLOR}>{self.h1count}) {message.replace(' & ', ' & ')}</{H1COLOR}></u>"))  # type: ignore
 
-    def printH2(self, message):
-        self.h2count += 1
+    def printH2(self, message: str) -> None:
+        self.h2count += 1  # type: ignore
         print()
-        print_formatted_text(HTML(f"<u><{H2COLOR}>{self.h1count}.{self.h2count}) {message.replace(' & ', ' &amp; ')}</{H2COLOR}></u>"))
+        print_formatted_text(HTML(f"<u><{H2COLOR}>{self.h1count}.{self.h2count}) {message.replace(' & ', ' & ')}</{H2COLOR}></u>"))  # type: ignore
 
-    def printDescription(self, content: list) -> None:
+    def printDescription(self, content: List[str]) -> None:
         content[0] = f"<{DESCRIPTIONCOLOR}>{content[0]}"
         content[len(content) - 1] = f"{content[len(content) - 1]}</{DESCRIPTIONCOLOR}>"
         print_formatted_text(HTML("\n".join(content)))
 
-    def printHighlight(self, message: str) -> None:
+    def printHighlight(self, message: str | List[str]) -> None:
         if isinstance(message, list):
             message = "\n".join(message)
 
-        print_formatted_text(HTML(f"<MediumTurquoise>{message.replace(' & ', ' &amp; ')}</MediumTurquoise>"))
+        print_formatted_text(HTML(f"<MediumTurquoise>{message.replace(' & ', ' & ')}</MediumTurquoise>"))
 
-    def printWarning(self, message):
-        print_formatted_text(HTML(f"<Red>Warning: {message.replace(' & ', ' &amp; ')}</Red>"))
+    def printWarning(self, message: str) -> None:
+        print_formatted_text(HTML(f"<Red>Warning: {message.replace(' & ', ' & ')}</Red>"))
 
     def printSummary(self, title: str, value: str) -> None:
         titleLength = len(title)
         message = f"{title} {'.' * (40 - titleLength)} {value}"
-        print_formatted_text(HTML(f"  <{SUMMARYCOLOR}>{message.replace(' & ', ' &amp; ')}</{SUMMARYCOLOR}>"))
+        print_formatted_text(HTML(f"  <{SUMMARYCOLOR}>{message.replace(' & ', ' & ')}</{SUMMARYCOLOR}>"))
 
     def printParamSummary(self, message: str, param: str) -> None:
-        if self.getParam(param) is None:
+        if self.getParam(param) is None:  # type: ignore
             self.printSummary(message, f"<{UNDEFINEDPARAMCOLOR}>Undefined</{UNDEFINEDPARAMCOLOR}>")
-        elif self.getParam(param) == "":
+        elif self.getParam(param) == "":  # type: ignore
             self.printSummary(message, f"<{UNDEFINEDPARAMCOLOR}>Default</{UNDEFINEDPARAMCOLOR}>")
         else:
-            self.printSummary(message, self.getParam(param))
+            self.printSummary(message, self.getParam(param))  # type: ignore
 
 
-def masPromptYesOrNo(message):
-    return HTML(f"<{PROMPTCOLOR}>{message.replace(' & ', ' &amp; ')}? [y/n]</{PROMPTCOLOR}> ")
+def masPromptYesOrNo(message: str) -> HTML:
+    return HTML(f"<{PROMPTCOLOR}>{message.replace(' & ', ' & ')}? [y/n]</{PROMPTCOLOR}> ")
 
 
-def masPromptValue(message):
-    return HTML(f"<{PROMPTCOLOR}>{message.replace(' & ', ' &amp; ')}</{PROMPTCOLOR}> ")
+def masPromptValue(message: str) -> HTML:
+    return HTML(f"<{PROMPTCOLOR}>{message.replace(' & ', ' & ')}</{PROMPTCOLOR}> ")
 
 
 class PromptMixin():
-    def yesOrNo(self, message: str, param: str = None) -> bool:
+    def yesOrNo(self, message: str, param: str | None = None) -> bool:
         response = prompt(message=masPromptYesOrNo(message), validator=YesNoValidator(), validate_while_typing=False)
         responseAsBool = response.lower() in ["y", "yes"]
 
         if param is not None:
-            self.params[param] = "true" if responseAsBool else "false"
+            self.params[param] = "true" if responseAsBool else "false"  # type: ignore
         return responseAsBool
 
-    def promptForString(self, message: str, param: str = None, default: str = "", isPassword: bool = False, validator: Validator = None, completer: WordCompleter = None) -> str:
+    def promptForString(self, message: str, param: str | None = None, default: str = "", isPassword: bool = False, validator: Validator | None = None, completer: WordCompleter | None = None) -> str:
         if param is not None and default == "":
             default = getenv(param.upper(), default="")
 
@@ -97,27 +98,30 @@ class PromptMixin():
             response = prompt(message=masPromptValue(message), is_password=isPassword, default=default, completer=completer, validator=validator, validate_while_typing=False)
 
         if param is not None:
-            self.params[param] = response
+            self.params[param] = response  # type: ignore
         return response
 
-    def promptForInt(self, message: str, param: str = None, default: int = None, min=None, max=None) -> int:
+    def promptForInt(self, message: str, param: str | None = None, default: int | None = None, min: int | None = None, max: int | None = None) -> int:
         if param is not None and default is None:
-            default = getenv(param.upper(), default=None)
+            default = getenv(param.upper(), default=None)  # type: ignore
 
         if default is None:
             response = int(prompt(message=masPromptValue(message), validator=IntValidator(min, max)))
         else:
             response = int(prompt(message=masPromptValue(message), validator=IntValidator(min, max), default=str(default)))
         if param is not None:
-            self.params[param] = str(response)
+            self.params[param] = str(response)  # type: ignore
         return response
 
-    def promptForListSelect(self, message: str, options: list, param: str = None, default: int = None) -> str:
+    def promptForListSelect(self, message: str, options: List[str], param: str | None = None, default: int | None = None) -> str:
         selection = self.promptForInt(message=message, default=default, min=1, max=len(options))
         # List indices are 0 origin, so we need to subtract 1 from the selection made to arrive at the correct value
-        self.setParam(param, options[selection - 1])
+        result = options[selection - 1]
+        if param is not None:
+            self.setParam(param, result)  # type: ignore
+        return result
 
-    def promptForFile(self, message: str, mustExist: bool = True, default: str = "", envVar: str = "") -> None:
+    def promptForFile(self, message: str, mustExist: bool = True, default: str = "", envVar: str = "") -> str:
         if default == "" and envVar != "":
             default = getenv(envVar, "")
         if mustExist:
@@ -125,8 +129,10 @@ class PromptMixin():
         else:
             return prompt(message=masPromptValue(message), default=default)
 
-    def promptForDir(self, message: str, mustExist: bool = True, default: str = "") -> None:
+    def promptForDir(self, message: str, mustExist: bool = True, default: str = "") -> str:
         if mustExist:
             return prompt(message=masPromptValue(message), validator=DirectoryExistsValidator(), validate_while_typing=False, default=default)
         else:
             return prompt(message=masPromptValue(message), default=default)
+
+# Made with Bob
