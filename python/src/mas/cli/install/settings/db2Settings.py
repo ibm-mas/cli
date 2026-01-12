bde78c811dc95b6d8b6c7e7caa93f5d6c7d82c85
jainyjoseph <jainy587@gmail.com>
2026-01-12 15:17:39 +0530

    retest case:unset var in catalog

--- python/src/mas/cli/install/settings/db2Settings.py
+++ python/src/mas/cli/install/settings/db2Settings.py
@@ -20,12 +20,12 @@ class Db2SettingsMixin():
             self.printH1("Configure Databases")
 
         # Set the default db2-Channel
-        # default_db2_channel = "v110509.0" #original
+        default_db2_channel = "v120101.0" #original
         # default_db2_channel = getattr(self, 'catalogDb2Channel', 'v110509.0')  # Get db2_channel from catalog metadata
-        if hasattr(self, 'chosenCatalog') and self.chosenCatalog:
-            default_db2_channel = self.chosenCatalog.get("db2_channel_default", "v110509.0")
-        else:
-            default_db2_channel = "v110509.0"
+        # if hasattr(self, 'chosenCatalog') and self.chosenCatalog:
+        #     default_db2_channel = self.chosenCatalog.get("db2_channel_default", "v110509.0")
+        # else:
+        #     default_db2_channel = "v110509.0"
         # Get user-specified value
         user_channel = self.getParam("db2_channel")
 
