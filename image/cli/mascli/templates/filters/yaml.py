e8e32169f83f77cf3ae6eef85a84b2a4dc740758
Andrew Whitfield <whitfiea@uk.ibm.com>
2026-02-03 08:17:41 +0000

    Update yaml.py

--- image/cli/mascli/templates/filters/yaml.py
+++ image/cli/mascli/templates/filters/yaml.py
@@ -6,7 +6,5 @@ def toYaml(value: str) -> dict:
     return yaml.safe_load(value)
 
 # Convert a Python object to a nicely formatted YAML string
-
-
 def to_nice_yaml(value, indent: int = 2) -> str:
     return yaml.dump(value, default_flow_style=False, indent=indent, sort_keys=False)
