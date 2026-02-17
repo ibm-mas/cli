77b40086603c4f93e09187bdfe032f8bc7103e33
KAROL CZARNECKI <karol.czarnecki@ie.ibm.com>
2026-02-17 09:49:21 +0000

    [patch] add get version for data science cluster - to display in fvt results

--- image/cli/app-root/src/finalizer.py
+++ image/cli/app-root/src/finalizer.py
@@ -66,8 +66,6 @@ def postMessage(channelName, messageBlocks, threadId=None):
 
 # Get Kafka Provider and Version
 # -------------------------------------------------------------------------
-
-
 def getKafkaVersion(namespace):
     try:
         crs = dynClient.resources.get(api_version="kafka.strimzi.io/v1beta2", kind="Kafka")
@@ -80,6 +78,19 @@ def getKafkaVersion(namespace):
         print(f"Unable to determine kafka version: {e}")
     return "unknown"
 
+# Get DataScience Version
+# -------------------------------------------------------------------------
+def getDscVersion():
+    try:
+        crs = dynClient.resources.get(api_version="datasciencecluster.opendatahub.io/v1", kind="DataScienceCluster")
+        cr = crs.get(name="default-dsc", namespace="opendatahub")
+        if cr.status and cr.status.release.version:
+            setObject["target.datascienceVersion"] = cr.status.release.version
+        else:
+            print("Unable to determine Data Science version: status.release.version unavailable")
+    except Exception as e:
+        print(f"Unable to determine Data Science version: {e}")
+    return "unknown"
 
 # Get cp4d components versions
 # -------------------------------------------------------------------------
@@ -549,6 +560,20 @@ if __name__ == "__main__":
             print("Unable to determine CP4D version: status.versions unavailable")
     except Exception as e:
         print(f"Unable to determine CP4D version: {e}")
+
+    # Lookup DataScience version
+    # -------------------------------------------------------------------------
+    try:
+        crs = dynClient.resources.get(api_version="datasciencecluster.opendatahub.io/v1", kind="DataScienceCluster")
+        cr = crs.get(name="default-dsc", namespace="opendatahub")
+        if cr.status and cr.status.release.version:
+            datascienceVersion = cr.status.release.version
+            setObject["target.datascienceVersion"] = datascienceVersion
+            getDscVersion()
+        else:
+            print("Unable to determine Data Science version: status.release.version unavailable")
+    except Exception as e:
+        print(f"Unable to determine Data Science version: {e}")
 
     # Connect to mongoDb
     # -------------------------------------------------------------------------
