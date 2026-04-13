25c690c44b89ff64e7057aabfec1481f64c84447
joshiricha444 <joshi.richa444@gmail.com>
2026-04-13 16:09:21 +0530

    Removed unsed params

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -57,6 +57,7 @@ class UpdateApp(BaseApp):
                 "skip_pre_check",
                 "dev_mode",
                 "cpd_product_version",
+                "aiservice_channel",
                 # Dev Mode
                 "artifactory_username",
                 "artifactory_token"
