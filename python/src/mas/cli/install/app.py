1c9bd0ac24b912b4938e0885a710490b38b817f7
Anil Prajapati <Anil.Prajapati@ibm.com>
2026-04-22 10:28:03 +0530

    [patch] add prepareAIServiceInstallSecrets for AI service

--- python/src/mas/cli/install/app.py
+++ python/src/mas/cli/install/app.py
@@ -67,6 +67,7 @@ from mas.devops.tekton import (
     updateTektonDefinitions,
     preparePipelinesNamespace,
     prepareInstallSecrets,
+    prepareAIServiceInstallSecrets,
     testCLI,
     launchInstallPipeline
 )
@@ -2139,6 +2140,16 @@ class InstallApp(BaseApp, InstallSettingsMixin, InstallSummarizerMixin, ConfigGe
                     slack_token=self.getParam("slack_token"),
                     slack_channel=self.getParam("slack_channel")
                 )
+
+                # If AI Service is being installed, also prepare AI Service Slack secrets
+                if self.getParam("aiservice_instance_id"):
+                    aiserviceInstanceId = self.getParam("aiservice_instance_id")
+                    prepareAIServiceInstallSecrets(
+                        dynClient=self.dynamicClient,
+                        instanceId=aiserviceInstanceId,
+                        slack_token=self.getParam("slack_token"),
+                        slack_channel=self.getParam("slack_channel")
+                    )
 
                 self.setupApprovals(pipelinesNamespace)
 
