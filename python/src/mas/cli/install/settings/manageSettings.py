771c7fd8366207d2b1d7d7cc094348ddf433ba55
SahilSheikh1 <68754786+SahilSheikh1@users.noreply.github.com>
2026-04-28 22:06:06 +0530

    [patch] removed extra line

--- python/src/mas/cli/install/settings/manageSettings.py
+++ python/src/mas/cli/install/settings/manageSettings.py
@@ -177,13 +177,12 @@ class ManageSettingsMixin():
                     self.params["mas_appws_components"] += ",aip=latest"
                 if self.yesOrNo(" - Vegetation Management"):
                     self.params["mas_appws_components"] += ",vegm=latest"
-                # Collaborate is only available in Manage 9.2 or higher 
+                # Collaborate is only available in Manage 9.2 or higher
                 manageChannel = self.getParam("mas_app_channel_manage")
                 if manageChannel and isVersionEqualOrAfter('9.2.0', manageChannel):
                     if self.yesOrNo(" - Collaborate"):
                         self.params["mas_appws_components"] += ",collaborate=latest"
                 logger.debug(f"Generated mas_appws_components = {self.params['mas_appws_components']}")
-
                 if ",icd=" in self.params["mas_appws_components"]:
                     self.printH2("Maximo IT License Terms")
                     self.printDescription([
