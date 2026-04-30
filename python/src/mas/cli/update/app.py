a5e4219a7e29b398a11d20ae6e763f0f93003668
terc1997 <64480693+terc1997@users.noreply.github.com>
2026-04-30 07:54:13 -0300

    [patch] consolidate slack and install secrets into prepareUpdateSecrets

--- python/src/mas/cli/update/app.py
+++ python/src/mas/cli/update/app.py
@@ -24,7 +24,7 @@ from mas.devops.data import getCatalog, getNewestCatalogTag
 from mas.devops.ocp import createNamespace, getConsoleURL, getClusterVersion, isClusterVersionInRange
 from mas.devops.mas import listMasInstances, getCurrentCatalog
 from mas.devops.aiservice import listAiServiceInstances
-from mas.devops.tekton import preparePipelinesNamespace, installOpenShiftPipelines, updateTektonDefinitions, launchUpdatePipeline, prepareUpdateSlackSecrets, prepareInstallSecrets
+from mas.devops.tekton import preparePipelinesNamespace, installOpenShiftPipelines, updateTektonDefinitions, launchUpdatePipeline, prepareUpdateSecrets
 from ..install.settings import AdditionalConfigsMixin
 
 
@@ -215,22 +215,12 @@ class UpdateApp(BaseApp, AdditionalConfigsMixin):
             with Halo(text=f'Preparing namespace ({pipelinesNamespace})', spinner=self.spinner) as h:
                 createNamespace(self.dynamicClient, pipelinesNamespace)
                 preparePipelinesNamespace(dynClient=self.dynamicClient)
-                prepareInstallSecrets(
+                prepareUpdateSecrets(
                     dynClient=self.dynamicClient,
-                    namespace=pipelinesNamespace,
+                    slack_token=self.getParam("slack_token"),
+                    slack_channel=self.getParam("slack_channel"),
                     db2LicenseFile=self.db2LicenseFileSecret,
                 )
-                h.stop_and_persist(symbol=self.successIcon, text=f"Namespace is ready ({pipelinesNamespace})")
-
-            # Create slack secret if slack token and channel are provided
-            if self.getParam("slack_token") and self.getParam("slack_channel"):
-                with Halo(text='Creating Slack notification secret', spinner=self.spinner) as h:
-                    prepareUpdateSlackSecrets(
-                        dynClient=self.dynamicClient,
-                        slack_token=self.getParam("slack_token"),
-                        slack_channel=self.getParam("slack_channel")
-                    )
-                    h.stop_and_persist(symbol=self.successIcon, text="Slack notification secret created")
 
             with Halo(text=f'Installing latest Tekton definitions (v{self.version})', spinner=self.spinner) as h:
                 updateTektonDefinitions(pipelinesNamespace, self.tektonDefsPath)
