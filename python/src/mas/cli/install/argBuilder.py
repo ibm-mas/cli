7c851e9f282d0d0982f581e5318b4304b3bc6b22
Sakshi Singhroha <Sakshi.Singhroha@ibm.com>
2026-05-06 14:16:55 +0530

    Revert non-GitOps files and apply autopep8 formatting

--- python/src/mas/cli/install/argBuilder.py
+++ python/src/mas/cli/install/argBuilder.py
@@ -107,6 +107,9 @@ class installArgBuilderMixin():
         if self.getParam('mas_configure_ingress').lower() == "true":
             command += f"  --configure-ingress{newline}"
 
+        if self.getParam('mas_use_service_mesh') != "":
+            command += f"  --servicemesh \"{self.getParam('mas_use_service_mesh')}\"{newline}"
+
         if self.getParam('mas_domain') != "":
             command += f"  --domain \"{self.getParam('mas_domain')}\"{newline}"
 
@@ -339,6 +342,9 @@ class installArgBuilderMixin():
                 command += f"  --aiservice-instance-id  \"{self.getParam('aiservice_instance_id')}\"{newline}"
             if self.getParam('aiservice_channel') != "":
                 command += f"  --aiservice-channel \"{self.getParam('aiservice_channel')}\"{newline}"
+
+            if self.getParam('aiservice_install_plan_approval') != "":
+                command += f"  --aiservice-install-plan-approval \"{self.getParam('aiservice_install_plan_approval')}\"{newline}"
 
             # Certificate Issuer for AI Service
             if self.getParam('aiservice_certificate_issuer') != "":
@@ -579,6 +585,8 @@ class installArgBuilderMixin():
         if self.getParam('approval_aiservice') != "":
             command += f"  --approval-aiservice \"{self.getParam('approval_aiservice')}\"{newline}"
 
+        if self.getParam('slack_channel') != "" and self.getParam('slack_token'):
+            command += f"  --slack-channel \"{self.getParam('slack_channel')}\"  --slack-token $SLACK_TOKEN{newline}"
         # More Options
         # -----------------------------------------------------------------------------
         if self.devMode:
