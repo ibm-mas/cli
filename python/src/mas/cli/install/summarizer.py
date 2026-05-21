3603b9d6863e939535db4a2225b693b1cb979671
Dixit Sathwara <Dixit.Sathwara1@ibm.com>
2026-05-21 22:51:07 +0530

    [patch] only show the apply rbac summary when you have the permission-mode define

--- python/src/mas/cli/install/summarizer.py
+++ python/src/mas/cli/install/summarizer.py
@@ -103,12 +103,13 @@ class InstallSummarizerMixin:
         self.printSummary("Operational Mode", operationalModeNames[self.operationalMode])
         if self.mas_permission_mode != "":
             self.printSummary("Permission Mode", self.mas_permission_mode)
+            # Only show "Apply Pre-Install MAS RBAC" when permission mode is defined
+            self.printSummary(
+                "Apply Pre-Install MAS RBAC",
+                "No" if self.skip_preinstall_rbac else "Yes",
+            )
         if self.getParam("mas_issuer_kind") != "":
             self.printParamSummary("Mas Certificate Issuer Kind", "mas_issuer_kind")
-        self.printSummary(
-            "Apply Pre-Install MAS RBAC",
-            "No" if self.skip_preinstall_rbac else "Yes",
-        )
         if self.isAirgap():
             self.printSummary("Install Mode", "Disconnected Install")
         else:
