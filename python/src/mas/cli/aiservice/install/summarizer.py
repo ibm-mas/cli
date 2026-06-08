145e719b4796bdd05c4e5bda6ad4528927036b8e
Aneri Thakkar <Aneri.Thakkar1@ibm.com>
2026-06-08 15:30:07 +0530

    [patch] remove admin mode ref from aiservice install tests

--- python/src/mas/cli/aiservice/install/summarizer.py
+++ python/src/mas/cli/aiservice/install/summarizer.py
@@ -46,8 +46,6 @@ class aiServiceInstallSummarizerMixin:
         self.printParamSummary("Release", "aiservice_channel")
         self.printParamSummary("Instance ID", "aiservice_instance_id")
         self.printParamSummary("Environment Type", "environment_type")
-        if self.admin_mode not in [None, ""]:
-            self.printSummary("Admin Mode", self.admin_mode)
 
         if "aiservice_certificate_issuer" in self.params:
             self.printParamSummary("Certificate Issuer", "aiservice_certificate_issuer")
