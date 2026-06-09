063a10529301b2299883143548c4f0aa64cceb5d
Gabriel Alexandre Bonamico <gabona@ibm.com>
2026-06-09 18:16:32 -0300

    [patch] fix black check

--- image/cli/app-root/src/finalizer.py
+++ image/cli/app-root/src/finalizer.py
@@ -391,7 +391,10 @@ if __name__ == "__main__":
         treatedComponents = {}
         for key, value in mobileComponents.items():
             if "mobileVersion" in value:
-                treatedComponents[key] = {"enabled": True, "version": (value["mobileVersion"] + " | " + value["buildToolsVersion"] +" | " + value["appProcessorVersion"])}
+                treatedComponents[key] = {
+                    "enabled": True,
+                    "version": (value["mobileVersion"] + " | " + value["buildToolsVersion"] + " | " + value["appProcessorVersion"]),
+                }
 
         setObject["products.ibm-mas-mobile.buildId"] = "NA"
         setObject["products.ibm-mas-mobile.buildNumber"] = "NA"
