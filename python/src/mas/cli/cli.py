2b11cf45b6a1a44bf327b9504e6ba89f7eac9c68
Jasmin Makwana <jasminmakwana1999@gmail.com>
2026-06-11 22:26:43 +0530

    Add upgrade_path for 9.2 release

--- python/src/mas/cli/cli.py
+++ python/src/mas/cli/cli.py
@@ -246,7 +246,8 @@ class BaseApp(PrintMixin, PromptMixin):
         }
 
         self.upgrade_path: Dict[str, str] = {
-            "9.1.x": "9.2.x-feature",
+            "9.2.x-feature": "9.2.x",
+            "9.1.x": "9.2.x",
             "9.1.x-feature": "9.1.x",
             "9.0.x": "9.1.x",
             "8.11.x": "9.0.x",
