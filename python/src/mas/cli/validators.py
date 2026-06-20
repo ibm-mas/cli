85a5c601087a596f709da9b93b5546e3a9a1dedf
David Parker <durera@gmail.com>
2026-06-20 15:01:25 +0100

    [patch] Linting

--- python/src/mas/cli/validators.py
+++ python/src/mas/cli/validators.py
@@ -231,14 +231,17 @@ class BucketPrefixValidator(Validator):
         bucketPrefix = document.text
 
         if not match(r"^.{1,4}$", bucketPrefix):
-            raise ValidationError(message='Bucket prefix does not meet the requirement', cursor_position=len(bucketPrefix))
+            raise ValidationError(message="Bucket prefix does not meet the requirement", cursor_position=len(bucketPrefix))
 
 
 class CustomizationArchiveNameValidator(Validator):
     def validate(self, document: Document) -> None:
         name = document.text
         if not match(r"^[0-9a-zA-Z][0-9a-zA-Z\-_.]+$", name):
-            raise ValidationError(message='Customization archive name must start with a letter or digit, and contain only letters, digits, hyphens, underscores, and dots', cursor_position=len(name))
+            raise ValidationError(
+                message="Customization archive name must start with a letter or digit, and contain only letters, digits, hyphens, underscores, and dots",
+                cursor_position=len(name),
+            )
 
 
 class NotEmptyValidator(Validator):
