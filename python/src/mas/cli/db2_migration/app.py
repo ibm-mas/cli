a2b7fc68a07cef014f5a827743c6d421a053ae5d
jainyjoseph <jainy587@gmail.com>
2026-06-26 12:23:25 +0530

    Updated branch #1

--- python/src/mas/cli/db2_migration/app.py
+++ python/src/mas/cli/db2_migration/app.py
@@ -4,7 +4,7 @@
 # (C) Copyright IBM Corp. 2026 All Rights Reserved.
 # US Government Users Restricted Rights - Use, duplication, or disclosure
 # restricted by GSA ADP Schedule Contract with IBM Corp.
-# ------------------------------------------------------------
+# -----------------------------------------------------------
 
 import logging
 
