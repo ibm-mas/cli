e8f7faab5a889f280763662f38534b60f15630d4
jainyjoseph <jainy587@gmail.com>
2026-08-07 18:00:54 +0530

    [patch] conflicts resolved

--- python/src/mas/cli/cli.py
+++ python/src/mas/cli/cli.py
@@ -1,4 +1,4 @@
-# *****************************************************************************
+# ******************************************************************************
 # Copyright (c) 2024, 2026 IBM Corporation and other Contributors.
 #
 # All rights reserved. This program and the accompanying materials
